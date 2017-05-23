5e5d0cd030741593cd5e7c25fa9bf6a3d14a4209
Allison Northrop <agnorthrop@gmail.com>
2017-05-23 15:23:05 -0700

    got the names of the trips to show up

---
+++ trek.js
@@ -0,0 +1,25 @@
+var url = 'https://trektravel.herokuapp.com/trips';
+
+var successCallback = function(response) {
+  console.log("Success!")
+  console.log(response);
+
+  var target = $('#trips');
+  for (var i = 0; i < response.length; i++) {
+    trip = response[i];
+    target.append("<li>" + trip['name'] + "</li>");
+  }
+}
+
+var failureCallback = function() {
+  console.log("Didn't work");
+  $("#errors").html("<h1>AJAX request failed!</h1>");
+}
+
+var clickHandler = function() {
+  $.get(url, successCallback) //passing a function around as a variable (to be invoted later.) when the request comes in, that is when it willb e called.
+};
+//as soon as the document is ready, load the page
+$(document).ready(function() {
+  $('#load').click(clickHandler);
+});
