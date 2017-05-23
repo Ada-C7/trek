cdead429fff6a795498594a6330b3d3db9c18405
add2point71dots <alcland@gmail.com>
2017-05-23 16:01:50 -0700

    ajax request is linked to button on home page but no trips displayed yet

--- js/trek.js
+++ js/trek.js
@@ -1,3 +1,15 @@
+var tripListUrl = 'https://trektravel.herokuapp.com/trips';
+
+var tripListSuccess = function(response) {
+  console.log('Success!');
+  console.log(response);
+};
+
+var tripClickHandler = function() {
+  $.get(tripListUrl, tripListSuccess);
+};
+
+
 $(document).ready(function() {
-  
+  $('#load-trips').click(tripClickHandler);
 });
