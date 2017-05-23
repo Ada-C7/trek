a58fa33ddffe9365171101c29a4d3c14aad76d92
Kayla <kubicke.kayla@gmail.com>
2017-05-23 15:23:50 -0700

    Basic file setup.

---
+++ index.js
@@ -0,0 +1,12 @@
+// You will build a web app that will query data from an api, using AJAX, to
+// display a list of trips and view more information on a specific trip.
+// While viewing a specific trip, you can claim your spot by sending data
+// back to the API.
+//
+// All of this should be done without ever having to reload the page. In 
+// other words, TREK will be a single page application and will only need one
+// html page (index.html).
+
+$(document).ready(function() {
+
+});
