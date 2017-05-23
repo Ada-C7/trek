c18ff76e75b306ba197b1a65b4d7b31ed4977a0e
add2point71dots <alcland@gmail.com>
2017-05-23 16:23:32 -0700

    trips generated when button is clicked

--- js/trek.js
+++ js/trek.js
@@ -1,12 +1,20 @@
 var tripListUrl = 'https://trektravel.herokuapp.com/trips';
+var tripListTemplate = _.template($('#trip-list-template').html());
 
 var tripListSuccess = function(response) {
   console.log('Success!');
   console.log(response);
+
+  for (i = 0; i < response.length; i++) {
+    var generatedHtml = tripListTemplate({
+      data: response[i]
+    });
+    $('#trips-list').append(generatedHtml);
+  }
 };
 
 var tripClickHandler = function() {
-  $.get(tripListUrl, tripListSuccess);
+  response = $.get(tripListUrl, tripListSuccess);
 };
 
 
