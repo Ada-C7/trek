764d013337c8c4e3a0d045fe7824e42273299b55
Lynn Trickey <lynn.trickey@gmail.com>
2017-05-23 16:54:26 -0700

    Added button click funciton to load all trips

--- javascript.js
+++ javascript.js
@@ -1,22 +1,23 @@
 // save response from API in an array
 var allTripsUrl = 'https://trektravel.herokuapp.com/trips';
 
-$(document).ready(function() {
+var sendToTemplate = function(response) {
   // compiles tripTemplate
   var tripsTemplate = _.template($('#trip-template').html());
+  var tripArray = response;
+  for (var i = 0; i < tripArray.length; i++) {
+    var generatedHtml = tripsTemplate({
+      trip: tripArray[i]
+    });
+    $('#trip-list').append($(generatedHtml));
+  }
+};
 
-  var sendToTemplate = function(response) {
-    console.log(response);
-    var tripArray = response;
-    for (var i = 0; i < tripArray.length; i++) {
-      var generatedHtml = tripsTemplate({
-        trip: tripArray[i]
-      });
-      $('#trip-list').append($(generatedHtml));
-    }
-  };
-  
-  // AJAX call to API, response sending to function sendToTemplate
+var tripsClickHandler = function(event) {
   $.get(allTripsUrl, sendToTemplate);
+};
 
+$(document).ready(function() {
+  // AJAX call to API, response sending to function sendToTemplate
+  $('#load').click(tripsClickHandler);
 });
