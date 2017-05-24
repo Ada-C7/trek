9592969088e71f7f9e1fc869392a0a9362834ae9
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-05-24 14:27:44 -0700

    final version of show/hide

--- main.js
+++ main.js
@@ -1,60 +1,55 @@
-
-
-$(document).ready(function(){
-  var urlTrips = "https://trektravel.herokuapp.com/trips";
+var successCallbackTrips = function(response){
   var tripsTemplate = _.template($('#trips-template').html());
 
-  var successCallbackTrips = function(response){
-    console.log("success");
-    console.log(response);
+  console.log("success");
+  console.log(response);
 
-    for (var i = 0; i < response.length; i++){
-      var generateHTML = tripsTemplate({
-        trip: response[i]
-      });
-
-      $('#trips-list').append($(generateHTML));
-      $('.hide-show-info').hide();
-    };
-  };
-
-  var successCallbackTrip = function(response){
-    console.log("success");
-    console.log(response);
-
+  for (var i = 0; i < response.length; i++){
     var generateHTML = tripsTemplate({
-      trip: response
+      trip: response[i]
     });
 
-
-    $('#'+ response.id).show();
-    $('#trip-' + response.id).replaceWith($(generateHTML));
-    $('button#show').hide();
+    $('#trips-list').append($(generateHTML));
+    $('.slidingInfo').hide();
   };
+};
 
-  var failureCallback = function() {
-    console.log("Didn't work :(");
-    $("#errors").html("<h2>Something went wrong - check back later</h2>");
-  };
+var successCallbackTrip = function(response){
+  var tripsTemplate = _.template($('#trips-template').html());
+  console.log("success");
+  console.log(response);
 
+  var generateHTML = tripsTemplate({
+    trip: response
+  });
+
+  $('#trip-' + response.id).replaceWith($(generateHTML));
+
+};
+
+var failureCallback = function() {
+  console.log("Didn't work :(");
+  $("#errors").html("<h2>Something went wrong - check back later</h2>");
+};
+
+$(document).ready(function(){
+
+  var urlTrips = "https://trektravel.herokuapp.com/trips";
+
+  // api request for all trips //
   $.get(urlTrips, successCallbackTrips).fail(failureCallback);
 
-// working on showing details for one trip //
-  $('#trips-list').on('click', '#show', function(event) {
+  // gets the info for one trip//
+  $('#trips-list').on('click', 'h3', function(event) {
     event.preventDefault();
-    console.log("someone clicked on a trip link");
-    // how to access the trip id
-    console.log(event.target.dataset.id)
 
     var urlTrip = urlTrips + "/" + event.target.dataset.id
     $.get(urlTrip, successCallbackTrip).fail(failureCallback);
   });
 
+  // will hide the extra info
   $('#trips-list').on('click', 'button#hide', function(event){
     event.preventDefault();
-    console.log("hide the info");
-    $('.hide-show-info').hide();
-    $('button#show').show();
+    $('.slidingInfo').hide();
   });
-
 });
