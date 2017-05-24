8e59aac97766c522ccc12cb17ebf6b7142a0d63f
add2point71dots <alcland@gmail.com>
2017-05-24 11:21:46 -0700

    can click on a trip to see its info

--- js/trek.js
+++ js/trek.js
@@ -1,11 +1,12 @@
-var baseUrl = 'https://trektravel.herokuapp.com/trips';
+var baseUrl = 'https://trektravel.herokuapp.com/trips/';
 var tripListTemplate = _.template($('#trip-list-template').html());
+var tripTemplate = _.template($('#trip-template').html());
 
 var tripListSuccess = function(response) {
   var tripList = $('<ul></ul>');
 
   for (i = 0; i < response.length; i++) {
-    var generatedHtml = tripListTemplate({
+    var generatedHtml = tripListTemplate( {
       data: response[i]
     });
     tripList.append(generatedHtml);
@@ -13,13 +14,29 @@ var tripListSuccess = function(response) {
   $('#trip-info').html(tripList);
 };
 
+var tripSuccess = function(response) {
+  console.log(response);
+  var generatedHtml = tripTemplate( {
+    data: response
+  });
+  $('#trip-info').html(generatedHtml);
+};
+
 var tripListClickHandler = function() {
-  response = $.get(baseUrl, tripListSuccess).fail(function() {
+  $.get(baseUrl, tripListSuccess).fail(function() {
     console.log('AJAX failed to load.');
     $('#errors').html('Couldn\'t load trips.');
   });
 };
 
+var tripClickHandler = function() {
+  var trip = $(this).attr('id');
+  var tripId = trip.substr(4, trip.length-1);
+
+  $.get(baseUrl + tripId, tripSuccess);
+};
+
 $(document).ready(function() {
   $('#load-trips').click(tripListClickHandler);
+  $('#trip-info').on('click', 'h4', tripClickHandler);
 });
