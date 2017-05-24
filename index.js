584c9cd59d4be86eab839d8cb3486725ca853065
sai samant <saisamant@gmail.com>
2017-05-24 13:47:48 -0700

    section to show one trip and reserve it (using a form)

---
+++ index.js
@@ -0,0 +1,85 @@
+var baseURL = " https://trektravel.herokuapp.com/trips";
+
+$(document).ready(function(){
+
+  $("#load").on('click', clickAllTrips);
+});
+
+
+var clickAllTrips = function(){
+
+    $.get(baseURL, successAllTrips).fail(failureCallback);
+  };
+
+var clickShowTrip = function(tripID) {
+  var showTripURL = baseURL + "/" + tripID;
+  $.get(showTripURL, successShowTrip).fail(failureCallback);
+};
+
+var clickReserveTrip = function(tripInfo) {
+  console.log(tripInfo);//submit button to do AJAX post
+  //https:trektravel.herokuapp.com/trips/1/reserve
+  //accepted params: name, age, email
+};
+
+
+var successShowTrip = function(response) {
+    $("#trips-list-header").empty();
+    $("#trips-list-body").empty();
+    var target = $('#show-trip');
+    keyNames = Object.keys(response);
+    var showTripTemplate = _.template($('#show-trip-template').html());
+
+    var generatedTripHTML = showTripTemplate({data: response, names: keyNames});
+    target.append($(generatedTripHTML));
+    console.log(keyNames);
+    console.log(response);
+
+    //form corresponds to an html tag - submit is the event handler for the submit button
+    $('form').submit(function(e) {
+      e.preventDefault(); //prevent default submit action
+      var url = $(this).attr("action")+ response.id +"/reserve";
+      var formData = $(this).serialize();
+      console.log("form data = ", formData);
+      $.post(url, formData, function(response){
+        $('#messages').html('<p> Trip reserved! </p>');
+        console.log(response);
+      }).fail(function(){
+        $("#message").html("<p>Faliure!</p>");
+      });
+  });
+};
+
+//only works for trip-list class. how make it work for all the classes (details and reserve)?
+var successAllTrips = function(response) {
+  $("#show-trip").empty();
+
+  //fill in the headers with the template
+  var tripHeaders = Object.keys(response[0]);
+  var targetHeaders = $('#trips-list-header');
+  var tripHeaderTemplate = _.template($('#trips-header-template').html());
+  var generatedHeaderHTML = tripHeaderTemplate({header: tripHeaders});
+  targetHeaders.append($(generatedHeaderHTML));
+
+  console.log("Success!");
+  console.log(response);
+
+  var targetTripList = $("#trips-list-body");
+  var listTemplate = _.template($('#trips-list-template').html());
+
+  response.forEach(function(trip) {
+    var generatedHTML = listTemplate({data: trip});
+    targetTripList.append($(generatedHTML));
+    //generate click handler for each API id and add API as an id to the <th> tag in the trip list
+    targetTripList.on("click", "#"+ trip.id, function () {
+      clickShowTrip(trip.id);
+    });
+  });
+};
+
+// Instead of binding events directly to the elements, bind one event to their container element, and delegate it:
+// In this case, the event handler is only executed for elements inside of the container #divID that have the class "pickup".
+
+var failureCallback = function() {
+  $("#errors").html("<h4>TREK request failed!</h4>");
+};
