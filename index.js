a28c3e3e645ab85692d47a86864d6b1a5269dfff
Hyunji Kim <ricecakemonster@gmail.com>
2017-05-24 11:55:40 -0700

    fixed retrieving individual trip info

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ var buildUrl = function(option){
 
 
 $(document).ready(function() {
-  // var tripsTemplate = _.template($('#trips-template').html());
-  $('#button').click(tripsClickHandler);
-  // console.log(tripsTemplate);
+  $("#button").click(tripsClickHandler);
 });
 
 
@@ -17,16 +15,44 @@ var tripsClickHandler = function(){
 }
 
 var tripsSuccessCallback = function(response){
-  var tripsTemplate = _.template($('#trips-template').html());
+  var tripsTemplate = _.template($("#trips-template").html());
   console.log(response[0]);
   for (var i = 0; i < response.length; i++) {
+
     var generatedHtml = tripsTemplate({
       data: response[i],
-      tripUrl: buildUrl(response[i].id)
+      // tripUrl: buildUrl(response[i].id)
     });
-    $('#all').append($(generatedHtml));
+    $("#display").append($(generatedHtml));
+
   }
+
+  for (var i = 0; i < response.length; i++) {
+  $("#trip"+response[i].id).click(function(){
+    alert("test");
+      var tripId = $(this).html().replace("Detail of Trip ", "");
+      var tripUrl = buildUrl(tripId);
+      console.log(tripUrl);
+
+      $.get(tripUrl, tripSuccessCallback).fail(failureCallback);
+
+  });
 };
+
+};
+
+var tripSuccessCallback = function(response){
+  console.log(response.id);
+
+  var tripTemplate = _.template($("#trip-template").html());
+  var generatedHtml = tripTemplate({
+    data: response
+  });
+  console.log("here");
+  $("#trip").append($(generatedHtml));
+
+  // for (var i = 0; i < )
+}
 
 var failureCallback = function() {
   console.log("Didn't work :(");
