e2a2cee0c2d20323dbfd6bf8b79e9e2efd0c41b9
Jackie Watanabe <sushiigirl@gmail.com>
2017-05-23 18:51:31 -0700

    got names of trips to load

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-// var buildUrl = function() {
-//   return "https://trektravel.herokuapp.com/trips";
-// };
-//
-//
-// var successCallback = function(response) {
-//   console.log("Success!");
-//   console.log(response);
-//
-//   $('#wonders').append(generated_html);
-// };
-//
-// var failureCallback = function() {
-//   console.log("Didn't work :(");
-//   $("#errors").html("<h1>AJAX request failed!</h1>");
-// };
+var buildUrl = function() {
+  return "https://trektravel.herokuapp.com/trips";
+};
+
+
+var successCallback = function(response) {
+  console.log("Success!");
+  console.log(response);
+
+  console.log($('#trips'));
+
+  var trips = [];
+  for (var i = 0; i < response.length; i++) {
+    var name = response[i].name;
+    $('#trips').append("<h1>" + name + "</h1>");
+  }
+
+
+};
+
+var failureCallback = function() {
+  console.log("Didn't work :(");
+  $("#errors").html("<h1>AJAX request failed!</h1>");
+};
+
+
+$(document).ready(function() {
+    var url = buildUrl();
+
+    $.get(url, successCallback).fail(failureCallback);
+
+    // var tripTemplate = _.template($('#trips-template').html());
+});
