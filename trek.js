d96153e110aab0d72585daf2fc4d2a54d121943e
EricaJCase <ericajcase@gmail.com>
2017-05-24 15:02:35 -0700

    detailed_trek_informatiom query working

--- trek.js
+++ trek.js
@@ -1,49 +1,59 @@
-var url = 'https://trektravel.herokuapp.com/trips';
+var url = 'https://trektravel.herokuapp.com/trips/';
 
-var successCallback = function (response) {
-  console.log("Success!");
-  var target = $("#vacations");
-  for (var i = 0; i < response.length; i++) {
-    var vacation = response[i];
-    target.append("<li>" +  vacation.continent + ": " + vacation.name+ " (" + vacation.weeks + " weeks)" + "</li>");
 
-  }
+
+var singleCallback = function (trekData){
+  var trekTemplate = _.template($('#detailed-trip-template').html());
+  
+  generatedHtml = trekTemplate({  data: trekData});
+  $('#trip_info').append($(generatedHtml));
 };
 
 var failureCallback = function () {
   $("#errors").html("<h1> AJAX request failed! </h1>");
 };
 
+var test = function (trekData)
+{
+  for (var i = 0; i < (trekData.length/2); i++) {
 
-// var clickHandler = function() {
-//   $.get(url, successCallback).fail(failureCallback);
-//
-// };
+    var generatedHtml = treksTemplate({
+      data: trekData[i],
+    });
+    $('#vacations').append($(generatedHtml));
+  }
 
-// Pretend we got this data from AJAX or something
+  $('.load').click(function(event){
+    id =  this.attributes["data-trip-id"].value;
+    show = url + id;
+    $.get(show,singleCallback);
+
+  });
+};
+
+
 
 $(document).ready(function() {
-  // $.get(url, successCallback).fail(failureCallback);
-  //associate the click handler
-  //no guaruntee
-
-
-
-  var trekTemplate = _.template($('#trek-item-template').html());
+  var treksTemplate = _.template($('#trek-item-template').html());
 
 
   var test = function (trekData)
   {
-    for (var i = 0; i < trekData.length; i++) {
-      console.log(trekData[i]);
+    for (var i = 0; i < (trekData.length/2); i++) {
 
-      var generatedHtml = trekTemplate({
+      var generatedHtml = treksTemplate({
         data: trekData[i],
       });
       $('#vacations').append($(generatedHtml));
     }
-  };
 
+    $('.load').click(function(event){
+      id =  this.attributes["data-trip-id"].value;
+      show = url + id;
+      $.get(show,singleCallback);
+
+    });
+  };
 
   $.get(url, test);
 });
