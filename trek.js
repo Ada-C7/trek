08cec56d825884b9c856cbcbdf895ec80312a42b
Janice Lichtman <jalichtman@gmail.com>
2017-05-24 11:12:18 -0700

    have list of trips showing on click , and a little color styling

---
+++ trek.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+
+ var url = "https://trektravel.herokuapp.com/trips";
+
+
+var tripsData = [];
+
+var successCallback = function(response) {
+  // console.log("Success!");
+  // console.log(response);
+
+  for (var i = 0; i < response.length; i++) {
+    tripsData.push(response[i]);
+  }
+  console.log("tripsDATA:"+tripsData[3]['name']);
+  var tripsListTemplate = _.template($("#trips-list-template").html());
+
+
+  for (var i = 0; i < tripsData.length; i++) {
+      var generatedHtml = tripsListTemplate({
+        data: tripsData[i]
+      });
+      $('#trips').append($(generatedHtml));
+    }
+};
+
+
+
+var failureCallback = function() {
+  console.log("Didn't work :(");
+  $("#errors").html("<h1>Could not get list of trips</h1>");
+};
+
+var clickHandler = function(event) {
+  $('#trips-title').append("Which of these exciting adventures are in your future?");
+  $.get(url, successCallback).fail(failureCallback);
+};
+
+$('#load-all-trips').click(clickHandler);
+
+
+});
