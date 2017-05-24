84fd29d259e81e450e23e537a50bbab6074ebd9d
Alix Hamilton <alix.hamilton@gmail.com>
2017-05-24 14:24:00 -0700

    can post reservations through form

--- trek.js
+++ trek.js
@@ -1,24 +1,32 @@
 var baseUrl = "https://trektravel.herokuapp.com/";
+var clearPage = function () {
+  $('#alert').empty();
+  $('#trip-list').empty();
+  $('#trip-details').empty();
+};
 
 $(document).ready(function () {
   var tripListTemplate = _.template($('#trip-list-template').html());
   var showTripTemplate = _.template($('#show-trip-template').html());
 
   var tripsCallback = function (response) {
-    $('#trip-details').empty();
+    clearPage();
     var generatedHtml = tripListTemplate({ trips: response });
     $('#trip-list').html(generatedHtml);
   };
 
   var tripCallback = function (response) {
-    $('#trip-list').empty();
-    var generatedHtml = showTripTemplate({ trip: response });
+    clearPage();
+    var reservationUrl = baseUrl + "trips/" + response.id + "/reserve";
+    var generatedHtml = showTripTemplate({
+      trip: response,
+      url: reservationUrl
+    });
     $('#trip-details').html(generatedHtml);
-    console.log(response);
   };
 
   var failCallback = function (response) {
-    $('main').html("<p>AJAX Request Failed!</p>");
+    $('#alert').html("<p>AJAX Request Failed!</p>");
   };
 
   $('#get-trips').click(function() {
@@ -29,5 +37,16 @@ $(document).ready(function () {
   $('#trip-list').on('click', 'a', function () {
     var url = baseUrl + "trips/" + $(this).attr('id');
     $.get(url, tripCallback).fail(failCallback);
+  });
+
+  $('#trip-details').on('click', "input[type='submit']", function(e) {
+    e.preventDefault();
+
+    var url = $('form').attr('action');
+    var formData = $('form').serialize();
+
+    $.post(url, formData, function (response) {
+      $("<p>Reserved Successfully!</p>").insertAfter('h3');
+    }).fail(failCallback);
   });
 });
