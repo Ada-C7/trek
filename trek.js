c9d8200ad8770ab7215535ea367ad59afce05b15
Allison Northrop <agnorthrop@gmail.com>
2017-05-24 10:14:17 -0700

    refactored to utilize underscore in html

--- trek.js
+++ trek.js
@@ -1,17 +1,21 @@
-var url = 'https://trektravel.herokuapp.com/trips';
+// var url = 'https://trektravel.herokuapp.com/trips';
+
+var tripUrl = 'https://trektravel.herokuapp.com/trips';
 
 var successCallback = function(response) {
   console.log("Success!")
   console.log(response);
 
-  var target = $('#trips');
-  for (var i = 0; i < response.length; i++) {
-    trip = response[i];
-    target.append("<h2>" + trip['name'] + "</h2>" +
-    "<ul>" + "<li>" + "Continent: " + trip['continent'] + "</li>" +
-    "<li>" + "Duration: " + trip['weeks'] + " weeks" + "</li>" + "</ul>");
-  }
-}
+  $(document).ready(function() {
+    var tripTemplate = _.template($('#trips-template').html());
+    for (var i = 0; i < response.length; i++) {
+      var generatedHtml = tripTemplate ({
+        data: response[i]
+      });
+      $("#trips-list").append(generatedHtml);
+    }
+  })
+};
 
 var failureCallback = function() {
   console.log("Didn't work");
@@ -19,9 +23,42 @@ var failureCallback = function() {
 }
 
 var clickHandler = function() {
-  $.get(url, successCallback) //passing a function around as a variable (to be invoted later.) when the request comes in, that is when it willb e called.
+  $.get(tripUrl, successCallback) //passing a function around as a variable (to be invoted later.) when the request comes in, that is when it willb e called.
 };
 //as soon as the document is ready, load the page
 $(document).ready(function() {
   $('#load').click(clickHandler);
 });
+
+
+
+
+
+//////////////////////////////////////////////////////
+// var successCallback = function(response) {
+//   console.log("Success!")
+//   console.log(response);
+//
+//   var target = $('#trips');
+//   // var names = $('.trip_name')
+//   // var trip_details = $('.trip_details')
+//   for (var i = 0; i < response.length; i++) {
+//     trip = response[i];
+//     target.append("<h2>" + trip['name'] + "</h2>" +
+//     "<ul>" + "<li>" + "Continent: " + trip['continent'] + "</li>" +
+//     "<li>" + "Duration: " + trip['weeks'] + " weeks" + "</li>" + "</ul>");
+//   }
+// }
+// //the trip name I need to link to by id
+// var failureCallback = function() {
+//   console.log("Didn't work");
+//   $("#errors").html("<h1>AJAX request failed!</h1>");
+// }
+//
+// var clickHandler = function() {
+//   $.get(trip_url, successCallback) //passing a function around as a variable (to be invoted later.) when the request comes in, that is when it willb e called.
+// };
+// //as soon as the document is ready, load the page
+// $(document).ready(function() {
+//   $('#load').click(clickHandler);
+// });
