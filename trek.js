16cd8117a86cdd67c8afe5f7f031b02eeee7d296
Olivia <ssecretsharer@gmail.com>
2017-05-24 15:38:18 -0700

    renamed callbacks, added show logic, organized logic into sections

--- trek.js
+++ trek.js
@@ -1,29 +1,63 @@
 
 
-//dan's solution to seven wonders
 
-  // Define the function
-  var successCallback = function(response) {
-    console.log("Trips!");
-    console.log(response);
 
-    $("#print-trippy").empty();
-    var allTripsTemplate = _.template($('#trippy').html());
-    for (var i = 0; i < response.length; i++) {
-      var allTripsHtml = allTripsTemplate({
-        trip: response[i]
-      });
-      $('#print-trippy').append($(allTripsHtml));
-    }
+//************ all trips function ****************
 
-  };
+var allCallback = function(response) {
+  console.log("Trips!");
+  console.log(response);
 
-  var failureCallback = function(response) {
-    console.log("No trippy for you:(");
+  var allfailureCallback = function(response) {
+    console.log("Error: No trippies for you:(");
     console.log(response);
   };
 
-//*************************************************
+  $("#print-trippy").empty();
+  var allTripsTemplate = _.template($('#trippy').html());
+  for (var i = 0; i < response.length; i++) {
+    var allTripsHtml = allTripsTemplate({
+      trip: response[i]
+    });
+    $('#print-trippy').append($(allTripsHtml));
+  }
+};
+
+
+
+//************* all trips event handling **************
+
+
+var allTripsClickHandler = function() {
+  var url = "https://trektravel.herokuapp.com/trips";
+  $.get(url, successCallback).fail(failureCallback);
+};
+
+
+//****************** show one trip function ************
+
+
+var showCallback = function(response) {
+  console.log("Trip!");
+  console.log(response);
+
+  var showfailureCallback = function(response) {
+    console.log("Error: No trippy for you:(");
+    console.log(response);
+  };
+
+  $("#print-trip").empty();
+  var showTripTemplate = _.template($('#trip').html());
+  for (var i = 0; i < response.length; i++) {
+    var showTripHtml = showTripTemplate({
+      trip: response[i]
+    });
+    $('#print-trip').append($(showTripHtml));
+  }
+};
+
+
+//**********************************************
 
 var showTripClickHandler = function() {
   console.log("clicker");
@@ -33,11 +67,7 @@ var showTripClickHandler = function() {
   $.get(showurl, tripCallback).fail(failureCallback);
 };
 
-
-var alltripsClickHandler = function() {
-  var url = "https://trektravel.herokuapp.com/trips";
-  $.get(url, successCallback).fail(failureCallback);
-};
+  //************* show one trip event handling **************
 
 
 $(document).ready(function() {
