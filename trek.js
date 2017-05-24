f485725529f6bad309449dcbe120b135107253c6
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-05-24 15:04:36 -0700

    wave 1 done

--- trek.js
+++ trek.js
@@ -1,3 +1,26 @@
-$(document).ready(function() {
+var url= "https://trektravel.herokuapp.com/trips";
 
+var trektravelTemplate = _.template($('#all-trips-template').html());
+
+var successCallback = function(response) {
+  var target = $('#all-trips');
+  for (var i = 0; i < response.length; i++) {
+    var generatedHTML = trektravelTemplate({
+      data: response[i]
+    });
+    target.append($(generatedHTML));
+  }
+};
+
+var failureCallback = function() {
+  $('#errors').html('<h2>Apologies, it appears the list of trips is not available right now. </h2>');
+};
+
+var clickHandler = function(event) {
+  $.get(url, successCallback).fail(failureCallback);
+};
+
+
+$(document).ready(function() {
+  $('#load').click(clickHandler);
 });
