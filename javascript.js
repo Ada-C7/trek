91603833d3d523364b5f2b3aa579a6825981093d
Lynn Trickey <lynn.trickey@gmail.com>
2017-05-23 19:52:15 -0700

    Added urls to template

--- javascript.js
+++ javascript.js
@@ -4,6 +4,7 @@ var allTripsUrl = 'https://trektravel.herokuapp.com/trips';
 var sendToTemplate = function(response) {
   // compiles tripTemplate
   var tripsTemplate = _.template($('#trip-template').html());
+
   var tripArray = response;
   for (var i = 0; i < tripArray.length; i++) {
     var generatedHtml = tripsTemplate({
