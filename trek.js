cd5eba5fa81b63e78b04b51cb6b170bd4a75c645
Alix Hamilton <alix.hamilton@gmail.com>
2017-05-24 11:06:07 -0700

    basic trip show details

--- trek.js
+++ trek.js
@@ -1,11 +1,20 @@
-url = "https://trektravel.herokuapp.com/trips";
+var baseUrl = "https://trektravel.herokuapp.com/";
 
 $(document).ready(function () {
   var tripListTemplate = _.template($('#trip-list-template').html());
+  var showTripTemplate = _.template($('#show-trip-template').html());
 
-  var successCallback = function (response) {
+  var tripsCallback = function (response) {
+    $('#trip-details').empty();
     var generatedHtml = tripListTemplate({ trips: response });
-    $('main').append(generatedHtml);
+    $('#trip-list').html(generatedHtml);
+  };
+
+  var tripCallback = function (response) {
+    $('#trip-list').empty();
+    var generatedHtml = showTripTemplate({ trip: response });
+    $('#trip-details').html(generatedHtml);
+    console.log(response);
   };
 
   var failCallback = function (response) {
@@ -13,6 +22,12 @@ $(document).ready(function () {
   };
 
   $('#get-trips').click(function() {
-    $.get(url, successCallback).fail(failCallback);
+    var url = baseUrl + "trips";
+    $.get(url, tripsCallback).fail(failCallback);
+  });
+
+  $('#trip-list').on('click', 'a', function () {
+    var url = baseUrl + "trips/" + $(this).attr('id');
+    $.get(url, tripCallback).fail(failCallback);
   });
 });
