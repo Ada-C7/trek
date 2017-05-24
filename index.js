3adc882d597347320f3a7d44578fc20c0b16d74e
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-05-24 12:46:30 -0700

    Show all trips button emptys content before reloading

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
-
 var tripsListCallback = function(response) {
+  $('#trips-list thead tr').append("<th>Name</th><th>Number of Weeks</th>");
   var tripsListTemplate = _.template($('#trips-list-template').html());
   for ( i = 0; i < response.length; i++){
-    var tripHTML = tripsListTemplate({
+    var newTripRow = tripsListTemplate({
       trip: response[i]
     });
-   $('#trips-list').append($(tripHTML));
+   $('#trips-list tbody').append($(newTripRow));
   }
 };
 
+var tripDetailsCallback = function() {
+};
 
 var failureCallback = function() {
   console.log("Didn't Work! :(");
   $("#errors").html("<h1>Request failed</h1>");
 };
 
+
 var tripsListClickHandler = function() {
+  $('#trips-list thead tr').empty();
+  $('#trips-list tbody').empty();
   url = "https://trektravel.herokuapp.com/trips";
   $.get(url, tripsListCallback).fail(failureCallback);
+};
+
+var tripDetailsClickHandler = function() {
+  url = "https://trektravel.herokuapp.com/trips";
+  $.get(url, tripDetailsCallback).fail(failureCallback);
 };
 
 $(document).ready(function() {
