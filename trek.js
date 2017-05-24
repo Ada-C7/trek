55891ca285718a46195f1098452ab1e7543f0806
AlisonZ <alisonkzerbe@gmail.com>
2017-05-24 15:56:04 -0700

    showing individual trip status and added foundation

--- trek.js
+++ trek.js
@@ -1,12 +1,43 @@
 var url = 'https://trektravel.herokuapp.com/trips';
-var successCallback = function(response){
-    console.log('Success!');
-    console.log(response);
-};
 
 var clickHandler = function(){
-    $.get(url, successCallback)
+    $.get(url, successCallback);
+};
+
+var successCallback = function(response){
+    console.log('Success!');
+    // console.log(response);
+
+    $('#trips-list').empty();
+
+    var tripsTemplate = _.template($('#trips-template').html());
+
+    for(var i = 0; i < response.length; i++){
+        var generatedHtml = tripsTemplate({
+            trip: response[i]
+        });
+        $('#trips-list').append($(generatedHtml));
+    }
+    $('.each-trip').click(showClickHandler)
+};
+
+var showCallback = function(info){
+    console.log('hi');
+    var showTemplate = _.template($('#show-template').html());
+
+    var individualTrip = showTemplate({
+        trip: info
+    });
+    $('#show-list').append($(individualTrip));
+
 }
+
+var showClickHandler = function(){
+    var showUrl = 'https://trektravel.herokuapp.com/trips/' + $(this).attr('id');
+    $.get(showUrl, showCallback);
+    $('#trips-list').empty();
+};
+
 $(document).ready(function(){
     $('#load').click(clickHandler);
 });
