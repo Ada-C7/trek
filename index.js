5eb7f13b100dacaeee5f648e6f83c420998fc1f9
Jackie Watanabe <sushiigirl@gmail.com>
2017-05-23 18:53:27 -0700

    got other information to load

--- index.js
+++ index.js
@@ -7,13 +7,36 @@ var successCallback = function(response) {
   console.log("Success!");
   console.log(response);
 
-  console.log($('#trips'));
+  // console.log($('#trips'));
 
-  var trips = [];
+
+  // var trips = [];
   for (var i = 0; i < response.length; i++) {
-    var name = response[i].name;
-    $('#trips').append("<h1>" + name + "</h1>");
+    var tripsTemplate = _.template($('#trips-template').html());
+
+    var generatedHtml = tripsTemplate({
+      data: response[i]
+    });
+
+    $('#trips-list').append($(generatedHtml));
+    // var name = response[i].name;
+    // $('#trips-template').append("<h1>" + name + "</h1>");
   }
+
+
+//   for (var i = 0; i < todoData.length; i++) {
+//   var generatedHtml = todoTemplate({
+//     data: todoData[i]
+//   });
+//   $('#todo-list').append($(generatedHtml));
+// }
+
+//   for (var i = 0; i < todoData.length; i++) {
+//   var generatedHtml = todoTemplate({
+//     data: todoData[i]
+//   });
+//   $('#todo-list').append($(generatedHtml));
+// }
 
 
 };
@@ -28,6 +51,8 @@ $(document).ready(function() {
     var url = buildUrl();
 
     $.get(url, successCallback).fail(failureCallback);
+
+    tripsTemplate = _.template($('#trips-template').html());
 
     // var tripTemplate = _.template($('#trips-template').html());
 });
