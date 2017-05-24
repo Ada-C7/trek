70d5f632c9b65df2379ee5a0d8b3570c906e0b99
Olivia <ssecretsharer@gmail.com>
2017-05-24 11:45:29 -0700

    First callbacks added, firs targeting and template attempts

--- trek.js
+++ trek.js
@@ -1,32 +1,32 @@
+
 $(document).ready(function() {
-  var tripTemplate = _.template($('#todo-item-template').html());
+  $('#trippy').click(alltripsClickHandler);
 });
 
+//dan's solution to seven wonders
+var successCallback = function(wonder) {
+  // Define the function
+  var successCallback = function(response) {
+    console.log("Trips!");
+    console.log(response);
 
-// Pretend we got this data from AJAX or something
-var tripData = [
-  {
-    name: 'Mow the lawn',
-    description: 'Must be finished before BBQ on Sat afternoon',
-    assignedTo: ['Kari', 'Charles']
-  }, {
-    title: 'Go to the Bank',
-    description: 'Need to make a transfer',
-    assignedTo: ['Dan', 'Jamie', 'Chris']
-  }, {
-    title: 'Tune the Piano',
-    description: 'High C is missing or something???',
-    assignedTo: ['Crystal']
-  }
-];
+  };
 
-$(document).ready(function() {
-  var todoTemplate = _.template($('#todo-item-template').html());
+  var failureCallback = function(response) {
+    console.log("No trippy for you:(");
+    console.log(response);
+  };
 
-  for (var i = 0; i < todoData.length; i++) {
-    var generatedHtml = todoTemplate({
-      data: todoData[i]
+  var target = $('#trippy');
+  var allTripsTemplate = _.template($('#trippy-template').html());
+  for (var i = 0; i < response.length; i++) {
+    var allTripsHtml = allTripsTemplate({
+      trip: response[i]
     });
-    $('#todo-list').append($(generatedHtml));
+    $('#trippy').append($(allTripsHtml));
   }
-});
+};
+
+
+
+//*************************************************
