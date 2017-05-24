9a84964697068266b825436a934d227964a4f91a
add2point71dots <alcland@gmail.com>
2017-05-24 10:14:52 -0700

    added foundation

--- js/trek.js
+++ js/trek.js
@@ -2,14 +2,15 @@ var tripListUrl = 'https://trektravel.herokuapp.com/trips';
 var tripListTemplate = _.template($('#trip-list-template').html());
 
 var tripListSuccess = function(response) {
-  $('#trips-list').empty();
+  var tripList = $('<ul></ul>');
 
   for (i = 0; i < response.length; i++) {
     var generatedHtml = tripListTemplate({
       data: response[i]
     });
-    $('#trips-list').append(generatedHtml);
+    tripList.append(generatedHtml);
   }
+  $('#trip-info').html(tripList);
 };
 
 var tripClickHandler = function() {
