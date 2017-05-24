0e692ea57dc21fdb6770658ee7c3439e921d7545
Cara Comfort <cara.comfort10@gmail.com>
2017-05-24 10:58:59 -0700

    Call trek API to show a list of all available trips

--- trek.js
+++ trek.js
@@ -1,1 +1,23 @@
-console.log("success!");
+const BASE_URL = "https://trektravel.herokuapp.com/trips";
+
+var successCallback = function(response) {
+  // console.log(response);
+
+  var trips = $('#trips');
+  response.forEach(function(trip) {
+    trips.append("<h3>" + trip['name'] + "</h3>");
+  });
+};
+
+var failureCallback = function() {
+  console.log("Unable to process request.");
+  $('#errors').html("<h1>Ajax request failed!</h1>")
+};
+
+var clickHandler = function() {
+  $.get(BASE_URL, successCallback).fail(failureCallback);
+};
+
+$(document).ready(function() {
+  $('#load').click(clickHandler);
+});
