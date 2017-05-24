4066ef2a4c3efc538fc9aa656d91d43c076c284c
Janice Lichtman <jalichtman@gmail.com>
2017-05-24 15:05:32 -0700

    indiv trip shows

--- trek.js
+++ trek.js
@@ -1,48 +1,73 @@
 $(document).ready(function() {
 
- var url = "https://trektravel.herokuapp.com/trips";
+  var url = "https://trektravel.herokuapp.com/trips";
 
 
-var tripsData = [];
+  var tripsData = [];
 
-var tripsSuccessCallback = function(response) {
-  $('#trips-title').html("Which of these exciting adventures are in your future?");
-  for (var i = 0; i < response.length; i++) {
-    tripsData.push(response[i]);
-  }
+  var tripsSuccessCallback = function(response) {
+    
+  var tripsTtitle = "Which of these exciting adventures are in your future?";
 
-  console.log(response);
-  var tripsListTemplate = _.template($("#trips-list-template").html());
+  $('#trips').append("<h2>"+tripsTtitle+"</h2>");
 
 
-  for (var i = 0; i < tripsData.length; i++) {
+    for (var i = 0; i < response.length; i++) {
+      tripsData.push(response[i]);
+    }
+
+    console.log(response);
+    var tripsListTemplate = _.template($("#trips-list-template").html());
+
+
+    for (var j = 0; j < tripsData.length; j++) {
       var generatedHtml = tripsListTemplate({
-        data: tripsData[i]
+        data: tripsData[j]
       });
       $('#trips').append($(generatedHtml));
     }
+  };
+
+
+  var tripsFailureCallback = function() {
+    console.log("Getting all trips did not work");
+    $("#errors").html("<h1>Sorry, we could not retrieve the list of trips at this time.</h1>");
+  };
+
+  var tripsClickHandler = function(event) {
+
+    $.get(url, tripsSuccessCallback).fail(tripsFailureCallback);
+  };
+
+
+
+  var singleTripSuccess= function(response) {
+
+    console.log(response);
+    var indivTripTemplate = _.template($("#indiv-trip-template").html());
+
+    var generatedHtml = indivTripTemplate({
+      data: response
+    });
+
+  $('#trips').hide();
+  $('#singleTrip').html($(generatedHtml));
+
+  };
+
+
+
+
+
+
+var singleTripFailure = function(){
+  console.log("Getting an individual trip did not work");
+  // $("#single-trip-errors").html("<h1>Sorry, we could not retrieve this trip at this time.</h1>");
 };
-
-
-var tripsFailureCallback = function() {
-  console.log("Getting all trips did not work");
-  $("#errors").html("<h1>Sorry, we could not retrieve the list of trips at this time.</h1>");
-};
-
-var tripsClickHandler = function(event) {
-
-  $.get(url, tripsSuccessCallback).fail(tripsFailureCallback);
-};
-
 
 var singleTripHandler = function(event){
- tripURL = url + "/" + $(this).attr("data-tripID");
- $.get(tripURL, singleTripSuccess);
- };
-
-var singleTripSuccess= function(response) {
-
-  console.log(response);
+  tripURL = url + "/" + $(this).attr("data-tripID");
+  $.get(tripURL, singleTripSuccess).fail(singleTripFailure);
 };
 
 
