5a9e9a6af2ef63feecdf2ccb6cb68965d138f00b
Leesyruiz <leesyruiz@gmail.com>
2017-05-23 21:41:40 -0700

    view all trips working

---
+++ trek.js
@@ -0,0 +1,28 @@
+var url = "https://trektravel.herokuapp.com/trips";
+
+var successCallback = function(response) {
+  console.log("Success!");
+  console.log(response);
+
+  var target = $("#trips");
+  for (var i = 0; i < response.length; i++) {
+    var trip = response[i];
+    target.append("<li>" +  trip["name"] +  "</li>");
+  }
+};
+
+var failureCallback = function() {
+  console.log("Didn't work :(");
+  $("#errors").html("<h1>AJAX request failed!</h1>");
+};
+
+var clickHandler = function() {
+  $.get(url, successCallback);
+  $.get(url, successCallback).fail(failureCallback);
+};
+
+$(document).ready(function() {
+  // Associate the click handler
+  $('#load').click(clickHandler);
+    // $.get(url, successCallback).fail(failureCallback);
+});
