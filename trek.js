f2f1c92ace5f603dcc249872cc541e94d3840d52
Leesyruiz <leesyruiz@gmail.com>
2017-05-24 12:13:30 -0700

    making process but its still not working ....

--- trek.js
+++ trek.js
@@ -1,61 +1,42 @@
-var url = "https://trektravel.herokuapp.com/trips";
-
-var successCallback = function(response) {
-  console.log("Success!");
-  console.log(response);
-
-  var target = $("#trip-list");
-  for (var i = 0; i < response.length; i++) {
-    var trip = response[i];
-    target.append("<p>" +  trip["name"] +  "</p>");
-  }
-};
-
-var failureCallback = function() {
-  console.log("Didn't work :(");
-  $("#errors").html("<h1>AJAX request failed!</h1>");
-};
-
-var clickHandler = function() {
-  $.get(url, successCallback);
-  $.get(url, successCallback).fail(failureCallback);
-};
+// var url = "https://trektravel.herokuapp.com/trips";
+//
+//
+//
+// var successCallback = function(response) {
+//   console.log("Success!");
+//   console.log(response);
+//
+//   var target = $("#trip-list");
+//   for (var i = 0; i < response.length; i++) {
+//     var trip = response[i];
+//     target.append("<p>" +  trip["name"] +  "</p>");
+//   }
+// };
+//
+//
+// var failureCallback = function() {
+//   console.log("Didn't work :(");
+//   $("#errors").html("<h1>AJAX request failed!</h1>");
+// };
+//
+// var clickHandler = function() {
+//   $.get(url, successCallback);
+//   $.get(url, successCallback).fail(failureCallback);
+// };
+//
+//
+//
+//
 //
 // $(document).ready(function() {
-//   // Associate the click handler
-//   $('#load').click(clickHandler);
-//     // $.get(url, successCallback).fail(failureCallback);
+//   $('#load').click(clickHandler); {
+//   };
 // });
-
-
-
-$(document).ready(function() {
-    $('#load').click(clickHandler); {
-
-var $trips = $("#trip-list");
-  $("#trip-list").find('p').on('click', function(e){
-  	e.preventDefault();
-
-var $desc = $('#trip-description');
-
-  // var tripTemplate = _.template($('#trip-template').html());
-  //
-  // // for (var i = 0; i < target.length; i++) {
-  //   var generatedHtml = tripTemplate({
-  //     // var target = $("#trip-list");
-  //     // for (var i = 0; i < response.length; i++) {
-  //       // var trip = response[i];
-  //       // target.append("<p>" +  trip["name"] +  "</p>");
-  //     tripID: trip["id"],
-  //     tripName: trip["name"],
-  //     tripDestination: trip["destination"],
-  //     tripContinent: trip["continent"],
-  //     tripAbout: trip["about"],
-  //     tripCategoty: trip["category"],
-  //     tripWeeks: trip["weeks"],
-  //     tripCost: trip["cost"];
-  //     console.log(tripTemplate);
-  //   });
-  //   // $('#trip-info-list').append($(generatedHtml));
-   }
-});
+//
+//
+//
+//
+// //
+// //  var $trips = $("#trip-list");
+// //    $("#trip-list").find('p').on('click', function(e){
+// //    	e.preventDefault();
