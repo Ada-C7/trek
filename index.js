cb11b71a2882a2a4027e90eec206adf2a5adac93
natalia-ku <bilberryfm@mail.ru>
2017-05-24 10:47:12 -0700

    adding css

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var url_all_trip = "https://trektravel.herokuapp.com/trips";
 
 $(document).ready(function() {
   $('#load').click(allTripsClickHandler);
+
   // checkIfTableEmpty();
   checkIfErrorsEmpty();
 });
@@ -35,38 +36,47 @@ var successTripCallback = function(response) {
     data: response
   });
   $('#trip').html($(generatedHtml));
-
+  // To change id of the trip in form:
   var id = response.id;
   $('form').attr('action','https://trektravel.herokuapp.com/trips/' + id + '/reserve');
-  //
-  // $('form').attr('action', function(i, v) {
-  //   return v + id;
-  //   console.log(v);
-  // });
-
-
+  // To create reservation inside trip show modal:
+  $('form').submit(function(e) {
+    e.preventDefault();
+    var url = $(this).attr("action");
+    var formData = $(this).serialize();
+    console.log(url);
+    $.post(url, formData, function(response){
+      $('#message').html('<p> You succesfully reserved your trip! </p>');
+      console.log(response);
+      console.log("I AM IN POST");
+    })
+    .fail(function(){
+      $('#errors').html('<p>Creating new reservation failed</p>');
+    });
+  });
 };
 
 
+// Failure callback for all actions:
 var failureCallback = function() {
   console.log("Didn't work :(");
   $("#errors").html("<p>AJAX request failed! Check your Internet connection</p>");
 };
 
 
-
-
+// Additional functions to check if table and error box is emty:
 var checkIfTableEmpty = function(){
-  // console.log($('#trips-list').html());
-  if (!$('#trips-list').html().includes("td")){
+  console.log($('#trips-list').html().includes("1"));
+  if (!$('#trips-list').html().includes("1")){
     $('#trips-list-table').hide();
   }
 };
 
-
-
 var checkIfErrorsEmpty = function(){
   if ($('#errors').html() == ""){
     $('#errors-block').hide();
+  }
+  if ($('#message').html() == ""){
+    $('#message-block').hide();
   }
 };
