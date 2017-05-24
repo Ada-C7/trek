542e7a4976e9c86903fdfab4a7c3f0ab737d2097
Spatterjaaay <aja.pursova@gmail.com>
2017-05-24 10:45:15 -0700

    added code to retrieve all trips

--- trek.js
+++ trek.js
@@ -1,2 +1,26 @@
+
+var url = 'https://trektravel.herokuapp.com/trips';
+
+var successCallback = function(response) {
+  console.log('success!');
+  console.log(response);
+  var target = $('#trips');
+
+  for (var i = 0; i < response.length; i++) {
+  var trip = response[i];
+  target.append("<li>" + trip.continent + ": " + trip.name + "</li>");
+}
+};
+
+var failureCallback = function(response) {
+  console.log('did not work');
+  $("#errors").html("<h1>AJAX request failed!</h1>");
+};
+
+var clickHandler = function(event) {
+  $.get(url, successCallback).fail(failureCallback);
+};
+
 $(document).ready( function() {
+  $("#load").click(clickHandler);
 });
