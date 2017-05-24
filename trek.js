07dbd96b6f634ce3c741191aa68a1189cc00f985
Spatterjaaay <aja.pursova@gmail.com>
2017-05-24 15:45:16 -0700

    added code and template to display information about a single trip

--- trek.js
+++ trek.js
@@ -2,8 +2,6 @@
 var url = 'https://trektravel.herokuapp.com/trips';
 
 var successCallback = function(response) {
-  // console.log('success!');
-  // console.log(response);
   var listTemplate = _.template($('#list-template').html());
   var target = $('#trips');
 
@@ -15,16 +13,35 @@ var successCallback = function(response) {
   }
 };
 
+var detailsSuccessCallback = function(response) {
+  var detailsTemplate = _.template($('#details-template').html());
+  var target = $('#trip-details');
+  var generatedHtml = detailsTemplate({
+      data: response
+  });
+
+  target.append($(generatedHtml));
+};
+
 var failureCallback = function(response) {
   console.log('did not work');
   $("#errors").html("<h1>AJAX request failed!</h1>");
 };
 
-var clickHandler = function(event) {
+var clickHandler = function() {
   $("#trips").empty();
   $.get(url, successCallback).fail(failureCallback);
 };
 
+var detailsClickHandler = function() {
+  var id = $(this).attr('id');
+  // console.log(id);
+  $("#trips").empty();
+  $("#trip-details").empty();
+  $.get(url + "/" + id, detailsSuccessCallback).fail(failureCallback);
+};
+
 $(document).ready( function() {
   $("#load").click(clickHandler);
+  $("#trips").on("click", "a", detailsClickHandler);
 });
