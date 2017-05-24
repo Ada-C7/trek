006e528dcabf891f011060ddbf93f56fd638369c
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-05-24 12:00:28 -0700

    have hide show function working for each item

--- main.js
+++ main.js
@@ -4,27 +4,38 @@ $(document).ready(function(){
   var urlTrips = "https://trektravel.herokuapp.com/trips";
   var tripsTemplate = _.template($('#trips-template').html());
 
-  var successCallback = function(response){
+  var successCallbackTrips = function(response){
     console.log("success");
     console.log(response);
-    console.log(typeof response);
-    // console.log(response.length);
 
-    if (response.length > 1) {
-      for (var i = 0; i < response.length; i++){
-        var generateHTML = tripsTemplate({
-          trip: response[i]
-        });
-        $('#trip-info').append($(generateHTML));
-      };
-
-    } else {
+    for (var i = 0; i < response.length; i++){
       var generateHTML = tripsTemplate({
-        trip: response
+        trip: response[i]
       });
-      $('#trip-info').empty();
+
       $('#trip-info').append($(generateHTML));
-    }
+      // will only hide the first one - adding div hides them all
+      // but showing will not show all
+      $('.hide-show-info').hide();
+    };
+  };
+
+  var successCallbackTrip = function(response){
+    console.log("success");
+    console.log(response);
+    console.log(response.id);
+    // console.log(event);
+
+    var generateHTML = tripsTemplate({
+      trip: response
+    });
+    // $('#trip-info').empty();
+
+    $('#'+response.id).show();
+    // $('.hide-show-info').show();
+
+
+    // $(this).html($(generateHTML));
   };
 
   var failureCallback = function() {
@@ -32,7 +43,7 @@ $(document).ready(function(){
     $("#errors").html("<h2>Something went wrong - check back later</h2>");
   };
 
-  $.get(urlTrips, successCallback).fail(failureCallback);
+  $.get(urlTrips, successCallbackTrips).fail(failureCallback);
 
 // working on showing details for one trip //
   $('#trip-info').on('click', 'a#trip', function(event) {
@@ -43,7 +54,7 @@ $(document).ready(function(){
     console.log(event.target.dataset.id)
     var urlTrip = urlTrips + "/" + event.target.dataset.id
     // console.log(urlTrip);
-    $.get(urlTrip, successCallback).fail(failureCallback);
+    $.get(urlTrip, successCallbackTrip).fail(failureCallback);
 
   });
 });
