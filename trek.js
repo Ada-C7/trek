e3183d175ecf9b6263548f73b5ac64f0aea3b187
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-05-23 17:01:12 -0700

    show page is working but not with dynamic ids

--- trek.js
+++ trek.js
@@ -5,29 +5,47 @@ var indexCallback = function(response) {
   var target = $('#content');
   var allTripsTemplate = _.template($('#all-trips-template').html());
   for (var i = 0; i < response.length; i++) {
-    var generatedHtml = allTripsTemplate({
+    var allTripsHTML = allTripsTemplate({
       trip: response[i]
     });
-    target.append($(generatedHtml));
+    target.append($(allTripsHTML));
   }
+  $('.get-trip').click(tripClickHandler);
+  
+};
+
+var tripCallback = function(response) {
+  console.log("Loaded a single trip!");
+  console.log(response);
+
+  var target = $('#content');
+  var singleTripTemplate = _.template($('#single-trip-template').html());
+  var oneTripHTML = singleTripTemplate({
+    thisTrip: response
+  });
+  target.append($(oneTripHTML));
 };
 
 var failureCallback = function() {
   console.log("Failed to get anything. Wooo....");
 };
 
-var clickHandler = function() {
+var indexClickHandler = function() {
   $("#content").empty();
   var url = "https://trektravel.herokuapp.com/trips";
   $.get(url, indexCallback).fail(failureCallback);
 };
 
+var tripClickHandler = function() {
+  console.log("clicked on a trip");
+  $("#content").empty();
+  var baseUrl = "https://trektravel.herokuapp.com/trips/1";
+  var id = "1";
+  var finalUrl = baseUrl + id;
+  $.get(finalUrl, tripCallback).fail(failureCallback);
+};
+
 $(document).ready(function() {
-
-  // declaring/compiling my templates
-  var singleTripTemplate = _.template($('#single-trip-template').html());
-  var allTripsTemplate = _.template($('#all-trips-template').html());
-
-  $('#get-trips').click(clickHandler);
-
+  $('#get-trips').click(indexClickHandler);
+  $('.get-trip').click(tripClickHandler);
 });
