45986eb37c8bb04e597a0b1e2047ce98633acb69
Andrea Valliere <amvalliere@gmail.com>
2017-05-24 11:10:11 -0700

    All trips button returning all trips from API and displaying in ul

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Click button to show all trips
+var url = "https://trektravel.herokuapp.com/trips";
+
+var successCallback = function(response) {
+  console.log("Success");
+  console.log(response);
+
+  var target = $("#trips");
+  for (var i = 0; i < response.length; i ++) {
+    var trip = response[i];
+    target.append("<li>" + trip['name'] + ": " + trip['continent'] + ": " + trip["weeks"] + " weeks");
+  }
+};
+
+var failureCallback = function() {
+  console.log("Nope");
+  $("#errors").html("<h1>AJAX request failed!</h1>");
+};
+
+var clickHandler = function(event) {
+  $.get(url, successCallback).fail(failureCallback)
+};
+
+$(document).ready(function() {
+  $('#load').click(clickHandler);
+});
