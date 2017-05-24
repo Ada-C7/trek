503c591bf2b218412d16589801448ba16fc14ed6
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-05-24 11:13:02 -0700

    api request for one trip working and showing details working

--- main.js
+++ main.js
@@ -6,14 +6,25 @@ $(document).ready(function(){
 
   var successCallback = function(response){
     console.log("success");
-    console.log(response)
+    console.log(response);
+    console.log(typeof response);
+    // console.log(response.length);
 
-    for (var i = 0; i < response.length; i++){
+    if (response.length > 1) {
+      for (var i = 0; i < response.length; i++){
+        var generateHTML = tripsTemplate({
+          trip: response[i]
+        });
+        $('#trip-info').append($(generateHTML));
+      };
+
+    } else {
       var generateHTML = tripsTemplate({
-        trip: response[i]
+        trip: response
       });
-      $('#trips-list').append($(generateHTML));
-    };
+      $('#trip-info').empty();
+      $('#trip-info').append($(generateHTML));
+    }
   };
 
   var failureCallback = function() {
@@ -24,11 +35,15 @@ $(document).ready(function(){
   $.get(urlTrips, successCallback).fail(failureCallback);
 
 // working on showing details for one trip //
-  $('#trips-list').on('click', 'a#trip', function(event) {
+  $('#trip-info').on('click', 'a#trip', function(event) {
     event.preventDefault();
     /*your_code_here;*/
     console.log("someone clicked on a trip link");
+    // how to access the trip id
+    console.log(event.target.dataset.id)
+    var urlTrip = urlTrips + "/" + event.target.dataset.id
+    // console.log(urlTrip);
+    $.get(urlTrip, successCallback).fail(failureCallback);
 
-    return false;
   });
 });
