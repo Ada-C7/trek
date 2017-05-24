2b4a610c089b475b862eb5b955eeb8cacfc985c7
Marisol Lopez <lmarisolopez@gmail.com>
2017-05-23 21:33:11 -0700

    initial code for successCallback function to get response and for button

---
+++ index.js
@@ -0,0 +1,23 @@
+var allTripsUrl = "https://trektravel.herokuapp.com/trips";
+
+var successCallback = function(response) {
+  console.log("Success!");
+  console.log(response);
+
+  var tripTemplate = _.template($('#trip-template').html());
+
+  for (var i = 0; i < response.length; i++) {
+    var generatedHtml = tripTemplate({
+      data: response[i]
+    });
+    $('#trip-list').append($(generatedHtml));
+  }
+};
+var clickHandler = function(event) {
+  $.get(allTripsUrl, successCallback);
+};
+
+
+$(document).ready(function() {
+  var response = $('#load-trips').click(clickHandler);
+});
