3103acb2532020b66501032265b9c65b22d13632
Jackie Watanabe <sushiigirl@gmail.com>
2017-05-23 17:16:29 -0700

    created initial files

---
+++ index.js
@@ -0,0 +1,16 @@
+// var buildUrl = function() {
+//   return "https://trektravel.herokuapp.com/trips";
+// };
+//
+//
+// var successCallback = function(response) {
+//   console.log("Success!");
+//   console.log(response);
+//
+//   $('#wonders').append(generated_html);
+// };
+//
+// var failureCallback = function() {
+//   console.log("Didn't work :(");
+//   $("#errors").html("<h1>AJAX request failed!</h1>");
+// };
