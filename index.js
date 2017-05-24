8ab9206d787c09099600f5fc2fb6c76652cd9666
amb54 <ambarklund@hotmail.com>
2017-05-24 15:45:36 -0700

    Updated the .ready block with an event for clicking the button#getTrip button. Subsequently created the functions clickHandlerTrip and successCallbackTrip. Created a function showPage to handle what parts to show at an event. Subsequently updated the functions successCallbackList and successCallbackTrip.

--- index.js
+++ index.js
@@ -1,23 +1,51 @@
 var urlBasic = "https://trektravel.herokuapp.com/trips";
 
+
+var showPage = function(page){
+  $('#list_head_of_trips').empty();
+  $('#list_of_trips').empty();
+  var list = document.getElementById("view_list");
+  var trip = document.getElementById("trip");
+  if (page === 'view_list'){
+    list.style.display = "block";
+    trip.style.display = "none";
+  }
+  if (page === 'trip'){
+    list.style.display = "none";
+    trip.style.display = "block";
+  }
+}
+
+
 var successCallbackList = function(response) {
   console.log("Success!");
   console.log(response);
-  $('#list_head_of_trips').empty();
-  $('#list_of_trips').empty();
+  showPage('view_list');
 
   var listHeadTemplate = _.template($('#list_head_template').html());
   $('#list_head_of_trips').html(listHeadTemplate);
 
   var listTemplate = _.template($('#list_template').html());
-
   for (var ind = 0; ind < response.length; ind++) {
+    var tripResponse = response[ind]
     var generatedHtml = listTemplate({
-      data: response[ind]
+      data: tripResponse
     });
     $('#list_of_trips').append($(generatedHtml));
-    // $('#getTrip').click(clickHandlerList);
   }
+};
+
+
+var successCallbackTrip= function(response){
+  console.log("Success! for the trip");
+  console.log(response);
+  showPage('trip');
+
+  var tripTemplate = _.template($('#trip_template').html());
+  var generatedHtml = tripTemplate({
+    data: response
+  });
+  $('#trip').html($(generatedHtml));
 };
 
 
@@ -32,7 +60,14 @@ var clickHandlerList = function(event) {
 };
 
 
+var clickHandlerTrip = function(event) {
+  var id = $(this).attr('data-tripid');
+  var url = urlBasic + '/' + id;
+  $.get(url, successCallbackTrip).fail(failureCallback);
+}
+
+
 $(document).ready(function() {
   $('#load_list').click(clickHandlerList);
-
+  $('#list_of_trips').on('click', 'button#getTrip' ,clickHandlerTrip);
 });
