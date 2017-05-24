8dde630bb6f8f7faccd55145d29006608c87530f
Ashtn <ashtonehorton@gmail.com>
2017-05-24 10:29:59 -0700

    can successfully call api to display a list trip names and locations

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
 $(document).ready(function(){
 
-
+$('#explore').click(indexClickHandler);
+// $('#trip-link').click(showClickHandler);
 });
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~ INDEX ~~~~~~~~~~~~~~~~~~~~~~~~ //
+
+var indexClickHandler = function(){
+     url = 'https://trektravel.herokuapp.com/trips';
+    $.get(url, indexSuccessCallback).fail(failureCallback);
+
+};
+
+
+var indexSuccessCallback = function(response){
+
+  var tripIndexTemplate = _.template($('#trip-index-template').html());
+
+  for(var i=0;i<response.length;i++){
+    var generatedHtml = tripIndexTemplate({
+      trip: response[i]
+    });
+    $('#trip-index').append($(generatedHtml));
+  }
+};
+
+
+var failureCallback = function(){
+
+};
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~ SHOW ~~~~~~~~~~~~~~~~~~~~~~~~ //
