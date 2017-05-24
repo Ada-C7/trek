7d10336e4f77aebc647918f597a96ebb02f63809
Olivia <ssecretsharer@gmail.com>
2017-05-24 14:44:34 -0700

    fixed display all, click handler and targeting

--- trek.js
+++ trek.js
@@ -1,14 +1,20 @@
 
-$(document).ready(function() {
-  $('#trippy').click(alltripsClickHandler);
-});
 
 //dan's solution to seven wonders
-var successCallback = function(wonder) {
+
   // Define the function
   var successCallback = function(response) {
     console.log("Trips!");
     console.log(response);
+
+    $("#print-trippy").empty();
+    var allTripsTemplate = _.template($('#trippy').html());
+    for (var i = 0; i < response.length; i++) {
+      var allTripsHtml = allTripsTemplate({
+        trip: response[i]
+      });
+      $('#print-trippy').append($(allTripsHtml));
+    }
 
   };
 
@@ -17,16 +23,23 @@ var successCallback = function(wonder) {
     console.log(response);
   };
 
-  var target = $('#trippy');
-  var allTripsTemplate = _.template($('#trippy-template').html());
-  for (var i = 0; i < response.length; i++) {
-    var allTripsHtml = allTripsTemplate({
-      trip: response[i]
-    });
-    $('#trippy').append($(allTripsHtml));
-  }
+//*************************************************
+
+var showTripClickHandler = function() {
+  console.log("clicker");
+  $("#tripper").empty();
+  var allTripsBaseUrl = "https://trektravel.herokuapp.com/trips/";
+  var showTripUrl = BaseUrl + $(this).attr('id');
+  $.get(showurl, tripCallback).fail(failureCallback);
 };
 
 
+var alltripsClickHandler = function() {
+  var url = "https://trektravel.herokuapp.com/trips";
+  $.get(url, successCallback).fail(failureCallback);
+};
 
-//*************************************************
+
+$(document).ready(function() {
+  $('#bon-voyage').click(alltripsClickHandler);
+});
