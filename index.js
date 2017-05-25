deeb4fabbcaa70d7f99aa7d9c01fc580c8a068c1
Danielle Birbal <birbalds@gmail.com>
2017-05-25 09:43:03 -0700

    Wave 2 Completed

--- index.js
+++ index.js
@@ -16,24 +16,68 @@ var tripData = function(response){
     var generatedHtml = tripTemplate({
         trip: response
       });
-      var sectionTarget = 'section#' + response['id']
 
-      if ($(sectionTarget)[0]){
-        $(sectionTarget).toggle();
-      } else {
-      var target = 'li#' + response['id']
-      $(target).append($(generatedHtml))
-    }
+      var sectionTarget = 'section#' + response['id'];
+      var target = 'li#' + response['id'];
+
+      $(target).after($(generatedHtml))
+
 
     console.log($('section#' + response['id']));
-    console.log('section#' + response['id']);
+    console.log($(sectionTarget).html());
 };
 
 var clickHandler = function(){
-      tripURL = url + "/" + this.id
-      console.log(this);
-      $.get(tripURL, tripData).fail(failureCallback);
+      var nextElement = $($('.oneTrip#'+this.id)[0]).next()[0];
+      if (nextElement.id === this.id) {
+        $(nextElement).remove();
+      } else {
+        tripURL = url + "/" + this.id;
+        console.log(this);
+
+        $.get(tripURL, tripData).fail(failureCallback);
+      }
 };
+
+$('form').submit(function(e) {
+  // By default, the form will attempt to do it's own local POST so we want to prevent that default behavior
+  e.preventDefault();
+
+  var url = $(this).attr("action"); // Retrieve the action from the form
+  var formData = $(this).serialize();
+
+  $.post(url, formData, function(response){
+    $('#message').html('<p> Reservation Made! </p>');
+
+    // What do we get in the response?
+    console.log(response);
+  });
+});
+
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+$('#reserveBtn').onclick = function() {
+    $('#popUpReserve').style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+$('span.close').onclick = function() {
+    $('#popUpReserve').style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == $('#popUpReserve')) {
+        $('#popUpReserve').style.display = "none";
+    }
+}
 
 $(document).ready(function() {
   tripTemplate = _.template($('#trip-template').html());
