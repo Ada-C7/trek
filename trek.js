abfda580201a6da16b1652c414b922d03e4d46e7
Allison Northrop <agnorthrop@gmail.com>
2017-05-25 14:01:58 -0700

    got the id's working with the description

--- trek.js
+++ trek.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   var tripUrl = 'https://trektravel.herokuapp.com/trips/';
 
-  var allTrips = function(response) {
+  var allTrips = function(response) { // callback function
     console.log("Success!")
     console.log(response);
     //new template, new event handler (get request to specific trip) from there it will append the template t the slide thing
@@ -15,12 +15,45 @@ $(document).ready(function() {
       $("#trips-list").append(generatedHtml);
     }
 
-
     $(".flip").click(function() {
-      $(".info").slideToggle("slow");
+      $("#trip-" + response.id).slideToggle("slow");
     });
   };
 
+  var trip = function(response) {
+    console.log("in the trip function!!");
+    console.log(response);
+    var tripTemplate = _.template($('#info-template').html());
+    // for (var i = 0; i < response.length; i++) {
+      var generatedHtml = tripTemplate ({
+        data: response
+      });
+      console.log("obj id: " + "#trip-" + response.id);
+      $("#trip-" + response.id).replaceWith($(generatedHtml)); //need to make a specific trip append to the specific name
+
+
+  };
+
+
+  var clickHandler = function() {
+    $.get(tripUrl, allTrips) //passing a function around as a variable (to be invoted later.) when the request comes in, that is when it willb e called.
+  };
+
+  $('#trips-list').on('click', '.trip', function(event){
+    console.log("success");
+    console.log(event);
+    console.log(this);
+
+    var indvTrips = tripUrl + event.target.dataset.id
+
+    $.get(indvTrips, trip) //event is a nasty object
+  });
+  //
+  // var clickHandler = function() {
+  //   tripInfoURL = tripUrl + data.id
+  //   $.get(tripInfoURL, allTrips)
+  // };
+  //as soon as the document is ready, load the page
 
   var failureCallback = function() {
     console.log("Didn't work");
@@ -28,10 +61,7 @@ $(document).ready(function() {
   }
 
 
-  var clickHandler = function() {
-    $.get(tripUrl, allTrips) //passing a function around as a variable (to be invoted later.) when the request comes in, that is when it willb e called.
-  };
-  //as soon as the document is ready, load the page
+
 
   $('#load').click(clickHandler);
 });
