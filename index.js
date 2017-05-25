56d246b0aa4f6d152f917eaf73cfdd769d9b6e78
Tamiko Terada <tamikodevo@gmail.com>
2017-05-25 10:24:14 -0700

    WIP: can post reservation but it all goes to same trip

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var baseUrl = "https://trektravel.herokuapp.com/trips";
 
+//TODO: use $().remove instead of .toggle
+
+// INDEX all trips--from Trek API
 var indexCallback = function(response) {
   console.log("Success!");
   console.log(response);
@@ -8,13 +11,13 @@ var indexCallback = function(response) {
 
   var tripTemplate = _.template($('#index-trip-template').html());
 
-  // each loop to create template for each trip object
+  // loop to create template for each trip object
   for (var i = 0; i < tripsData.length; i ++) {
     var index_template = tripTemplate( { data: tripsData[i] }); //rename
     $('#trips').append(index_template);
   }
 
-  $(".show").mousedown(function(event) {
+  $(".show").click(function(event) {
     var url = "";
     url = baseUrl + "/" + this.innerHTML;
     $.get(url, showCallback).fail(failureCallback);
@@ -22,6 +25,7 @@ var indexCallback = function(response) {
   });
 };
 
+// SHOW each trip--id grabbed from button html
 var showCallback = function(response) {
   console.log("Success!");
   console.log(response);
@@ -30,13 +34,20 @@ var showCallback = function(response) {
 
   var showTemplate = _.template($('#show-trip-template').html());
   var showHtml = showTemplate( { data: tripData } );
-    $('#show-page').html(showHtml);
+  $('#show-page').html(showHtml);
+
+  $(".reserve-start").click(function(event) {
+    var reserveTemplate = _.template($('#reserve-trip-template').html());
+    $('#show-page').append(reserveTemplate);
+  });
 };
 
 var failureCallback = function() {
   console.log("Fail :(");
   $("#errors").html("<h1>Your AJAX request failed!</h1>");
 };
+
+//***********************ONCE DOM IS READY************************
 
 $(document).ready(function() {
   $("#trips").toggle(false); //to turn trips 'off' on load
