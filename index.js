ebeee0ecc47d0d10c8cd72d17f848d887d11c954
Marisol Lopez <lmarisolopez@gmail.com>
2017-05-25 15:46:48 -0700

    wave 2

--- index.js
+++ index.js
@@ -1,43 +1,56 @@
 var allTripsUrl = "https://trektravel.herokuapp.com/trips/";
 
-var successTripsCallback = function(response) {
-  console.log("Success!");
-  // console.log(response);
-
-  var tripTemplate = _.template($('#trips-template').html());
-
-  for (var i = 0; i < response.length; i++) {
-    var generatedHtml = tripTemplate({
-      data: response[i]
-    });
-    $('#trip-list').append($(generatedHtml));
-  }
-  $("[href]").click(function(event){
-    $("p").slideToggle();
-    console.log(event)
-  });
-};
-
-var clickHandler = function(event) {
-  $.get(allTripsUrl, successTripsCallback);
-  // console.log(event)
-};
-
-// var individualTripURL = allTripsUrl + data.id
-
-
-var successIndividualTripCallBack = function(response) {
-  var tripTemplate = _.template($('#individual-trip').html());
-  // grab it by id, based on what the user clicks, which i can grab from the event properties
-}
-
-
-
-// var toggleDown = function(event) {
-//   $.get(singleTripUrl, data(?));
-// }
-
 $(document).ready(function() {
+  var successTripsCallback = function(response) {
+    console.log("Success!");
+    //here i am compiling my template
+    var tripTemplate = _.template($('#trips-template').html());
+    //loop that is parsing through all trips api and saving the response in generatedHtml, which I'm doing because I'll call it in my template.
+    for (var i = 0; i < response.length; i++) {
+      var generatedHtml = tripTemplate({
+        data: response[i]
+      });
+      //here i am appending my response which i've housed in y generated Html variable and appending it to '#trip-list'
+      $('#trip-list').append($(generatedHtml));
+    }
+    $(".trip-link").click(function(event){
+      var tripId = $(this).attr("data-trip-id");
+      // console.log("I'm inside click handler for a trip in the list for trip " + tripId);
+      var individualTripURL = allTripsUrl + tripId;
+      $.get(individualTripURL, successIndividualTripCallBack);
+    });
+
+  };
+
+  var successIndividualTripCallBack = function(response) {
+    console.log("I'm in my successIndividualTripCallBack function");
+    console.log(response);
+    var tripDetailsTemplate = _.template($('#individual-trip-template').html());
+    var generatedHtml = tripDetailsTemplate({
+      data: response
+    });
+    $("#individual-trip").html($(generatedHtml));
+  };
+
+
+
+
+  // grab it by id, based on what the user clicks, which i can grab from the event properties
+
+
+
+
+  var clickHandler = function(event) {
+    $.get(allTripsUrl, successTripsCallback);
+    // console.log(event)
+  };
+
+  // var individualTripClick
+  // var toggleDown = function(event) {
+  //   $.get(singleTripUrl, data(?));
+  // }
+
+
   $('#load-trips').click(clickHandler);
   // $('[href]').slideToggle(toggleDown);
 
