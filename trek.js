27d88a233f50dcd2f5c011d8afda24d43e6f2141
Olivia <ssecretsharer@gmail.com>
2017-05-24 22:07:42 -0700

    additional event handling template match up

--- trek.js
+++ trek.js
@@ -8,11 +8,6 @@ var allCallback = function(response) {
   console.log("Trips!");
   console.log(response);
 
-  var allfailureCallback = function(response) {
-    console.log("Error: No trippies for you:(");
-    console.log(response);
-  };
-
   $("#print-trippy").empty();
   var allTripsTemplate = _.template($('#trippy').html());
   for (var i = 0; i < response.length; i++) {
@@ -21,55 +16,54 @@ var allCallback = function(response) {
     });
     $('#print-trippy').append($(allTripsHtml));
   }
+  $("#detail-buttons").click(showTripClickHandler);
 };
 
-
+var allfailureCallback = function(response) {
+  console.log("Error: No trippies for you:(");
+  console.log(response);
+};
 
 //************* all trips event handling **************
 
-
 var allTripsClickHandler = function() {
   var url = "https://trektravel.herokuapp.com/trips";
-  $.get(url, successCallback).fail(failureCallback);
+  $.get(url, allCallback).fail(allfailureCallback);
 };
 
 
 //****************** show one trip function ************
 
-
 var showCallback = function(response) {
   console.log("Trip!");
   console.log(response);
 
+  // $("#tripper").empty();
+  var showTripTemplate = _.template($('#tripper').html());
+  var showTripHtml = showTripTemplate({
+      tripper: response
+    });
+    $('#print-tripper').append($(showTripHtml));
+    // $('#new-trip-form').submit(newTripClickHandler);
+    };
+
   var showfailureCallback = function(response) {
-    console.log("Error: No trippy for you:(");
+    console.log("Sorry, could not find this trippy:(");
     console.log(response);
   };
 
-  $("#print-trip").empty();
-  var showTripTemplate = _.template($('#trip').html());
-  for (var i = 0; i < response.length; i++) {
-    var showTripHtml = showTripTemplate({
-      trip: response[i]
-    });
-    $('#print-trip').append($(showTripHtml));
-  }
-};
-
-
-//**********************************************
-
-var showTripClickHandler = function() {
-  console.log("clicker");
-  $("#tripper").empty();
-  var allTripsBaseUrl = "https://trektravel.herokuapp.com/trips/";
-  var showTripUrl = BaseUrl + $(this).attr('id');
-  $.get(showurl, tripCallback).fail(failureCallback);
-};
-
   //************* show one trip event handling **************
+
+  var showTripClickHandler = function() {
+
+    var allTripsBaseUrl = "https://trektravel.herokuapp.com/trips/";
+    var showTripUrl = allTripsBaseUrl + $(this).attr('id');
+    $.get(showTripUrl, showCallback).fail(showfailureCallback);
+  };
 
 
 $(document).ready(function() {
-  $('#bon-voyage').click(alltripsClickHandler);
+  $('#bon-voyage').click(allTripsClickHandler);
+  $('#detail-buttons').click(showTripClickHandler);
+  // $('#new-trip').click(newTripClickHandler);
 });
