90c6be1fe25aca771d0023e04d1a1ec0b37b18db
Cara Comfort <cara.comfort10@gmail.com>
2017-05-25 10:21:12 -0700

    Add reserve spot button so form only revelead upon signup

--- trek.js
+++ trek.js
@@ -46,6 +46,15 @@ var popupHandler = function() {
 };
 
 $(document).ready(function() {
+  $("form").hide();
+
+  $("#reserve-spot").click(function(){
+    // $("form").toggle();
+    $("form").show();
+    $("#reserve-spot").hide();
+
+  });
+
   $('#load-trips').click(clickHandler);
 
   $('#trips').on('click', 'li', function() {
@@ -64,25 +73,7 @@ $(document).ready(function() {
       $('#errors').html('<h3> Spot reserved! </h3>');
       console.log(response);
     }).fail(function() { $('#errors').html('<h3> Unable to reserve spot at this time. </h3>')})
-
   });
-
-  // $('form').submit(function(e) {
-  // // By default, the form will attempt to do it's own local POST so we want to prevent that default behavior
-  // e.preventDefault();
-  //
-  // var url = $(this).attr("action"); // Retrieve the action from the form
-  // var formData = $(this).serialize();
-  //
-  // $.post(url, formData, function(response){
-  //   $('#message').html('<p> Pet added! </p>');
-  //
-  //   // What do we get in the response?
-  //   console.log(response);
-  // }).fail(function(){
-  //   $('#message').html('<p>Adding Pet Failed</p>');
-  // });
-  // });
 
   // $('#popup-link').on('click', function(event)  {
   //     event.preventDefault();
