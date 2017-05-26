3811127285768cafd1e425830a118ce8e4277fb3
Olivia <ssecretsharer@gmail.com>
2017-05-26 11:50:09 -0700

    reserve trip functionality working i think

--- trek.js
+++ trek.js
@@ -1,5 +1,4 @@
 
-
 window.addEventListener("load", function(e) {
 
 });
@@ -34,67 +33,54 @@ var allTripsClickHandler = function() {
   $.get(url, allCallback).fail(allfailureCallback);
 };
 
-
 //****************** show one trip function ************
 
 var showCallback = function(response) {
   console.log("Trip!");
   console.log(response);
 
-    $("#print-tripper").empty();
-    $("#print-trippy").empty();
+  $("#print-tripper").empty();
+  $("#print-trippy").empty();
   var showTripTemplate = _.template($('#tripper').html());
   var showTripHtml = showTripTemplate({
-      thisTrip: response
-    });
-    $('#print-tripper').append($(showTripHtml));
-    // $('#new-trip-form').submit(newTripClickHandler);
-    };
+    thisTrip: response
+  });
+  $('#print-tripper').append($(showTripHtml));
+  // $('#new-trip-form').submit(newTripClickHandler);
+};
 
-  var showfailureCallback = function(response) {
-    console.log("Sorry, could not find this trippy:(");
-    console.log(response);
-  };
+var showfailureCallback = function(response) {
+  console.log("Sorry, could not find this trippy:(");
+  console.log(response);
+};
 
-  //************* show one trip event handling **************
+//************* show one trip event handling **************
 
-  var showTripClickHandler = function() {
+var showTripClickHandler = function() {
 
-    var allTripsBaseUrl = "https://trektravel.herokuapp.com/trips/" + this.id;
-    console.log(this.id);
-    $.get(allTripsBaseUrl, showCallback).fail(showfailureCallback);
-  };
+  var allTripsBaseUrl = "https://trektravel.herokuapp.com/trips/" + this.id;
+  console.log(this.id);
+  $.get(allTripsBaseUrl, showCallback).fail(showfailureCallback);
+};
 
-  //***************** reserve trip **********************************
-//
-//
-//   var resClickHandler = function(event) {
-//     $("#errors").empty();
-//     event.preventDefault();
-//     console.log("reserved!");
-//     $("#reserve").empty();
-//     var data = $(this).serialize();
-//     console.log(data);
-//     var reservationBaseUrl = "https://trektravel.herokuapp.com/trips/";
-//     var reservationUrl = reservationBaseUrl + $('#member-id').html() + "/reserved";
-//     $.post(reservationUrl, data, reserveCallback).fail(failureCallback);
-//   };
-//
-//*************** new trip *************************
-//
-//
+//***************** reserve trip **********************************
 
-//****************************************
-
-
-
+var resClickHandler = function(event) {
+  $("#errors").empty();
+  event.preventDefault();
+  console.log("reserved!");
+  $("#reserve").empty();
+  var data = $(this).serialize();
+  console.log(data);
+  var reservationBaseUrl = "https://trektravel.herokuapp.com/trips/";
+  var reservationUrl = reservationBaseUrl + $('#reservation-id').html() + "/reserved";
+  $.post(reservationUrl, data, reserveCallback).fail(resfailureCallback);
+};
 
 $(document).ready(function() {
   $('#bon-voyage').click(allTripsClickHandler);
   // $('.detail-buttons').click(showTripClickHandler);
   // $('a').addClass('animated-link');
   // $("li").addClass(function(index) {
-  //   $(this).addClass("item-" + index);
-  // });
-  // $('#new-trip').click(newTripClickHandler);
+
 });
