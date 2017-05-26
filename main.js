55e47c18b59bdd39d60baf2de75faf6687de1feb
Ting Wong <hellotingwong@gmail.com>
2017-05-26 10:00:25 -0700

    added display form functionality

--- main.js
+++ main.js
@@ -2,18 +2,37 @@ var url = "https://trektravel.herokuapp.com/trips/";
 
 // tripTemplate compiled at document ready (called only once);
 var tripTemplate;
+var singleTripTemplate;
+var displayFormTemplate;
+
+$('#trip-list').hide();
+
+var singleTripCallback = function(response) {
+  console.log("Success!");
+  console.log(response);
+  $('#trip-list').hide();
+  $('#load').hide();
+  var generatedHTML = singleTripTemplate({
+    trip: response
+  });
+  $('#single-trip-detail').html(generatedHTML);
+  $('#all-trips-button').click(clickHandler);
+  $('#display-form').click(displayFormHandler);
+};
 
 var successCallback = function(response) {
   console.log("Success!");
   console.log(response);
+  $('#load').hide();
+  $('#trip-list').show();
 
   for (var i = 0; i < response.length; i++){
     var generatedHTML = tripTemplate({
-      trip: response[i],
-      link: url + response[i]['id']
+      trip: response[i]
     });
     $('#trip-list tbody').append(generatedHTML);
   }
+  $('.trip-details').click(tripDetailHandler);
 };
 
 var failureCallback = function() {
@@ -25,7 +44,21 @@ var clickHandler = function(event) {
   $.get(url, successCallback).fail(failureCallback);
 };
 
+var tripDetailHandler = function(event){
+  var tripID = $(this).attr("data-trip-id");
+  var detailURL = url + tripID;
+  $.get(detailURL, singleTripCallback).fail(failureCallback);
+};
+
+var displayFormHandler = function(event){
+  // var tripID = $(this).attr("data-trip-id");
+  var generatedHTML = displayFormTemplate({});
+  $('#display-form').html(generatedHTML);
+};
+
 $(document).ready(function() {
   tripTemplate = _.template($('#trip-template').html());
+  singleTripTemplate = _.template($('#single-trip-template').html());
+  displayFormTemplate = _.template($('#display-form-template').html());
   $('#load').click(clickHandler);
 });
