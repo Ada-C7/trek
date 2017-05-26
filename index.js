e488b6c4836cedcc8d583ed48d582f05bbbb96b5
Tamiko Terada <tamikodevo@gmail.com>
2017-05-26 11:58:27 -0700

    trip reservation now posting

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ var baseUrl = "https://trektravel.herokuapp.com/trips";
 
 //TODO: use $().remove instead of .toggle
 
+var failureCallback = function() {
+  console.log("Fail :(");
+  $("#errors").html("<h1>Your AJAX request failed!</h1>");
+};
+
 // INDEX all trips--from Trek API
 var indexCallback = function(response) {
   console.log("Success!");
@@ -18,42 +23,53 @@ var indexCallback = function(response) {
   }
 
   $(".show").click(function(event) {
-    var url = "";
-    url = baseUrl + "/" + this.innerHTML;
-    $.get(url, showCallback).fail(failureCallback);
+    var show_url = "";
+    show_url = baseUrl + "/" + this.innerHTML.toString(); // id of trip
+    $.get(show_url, showCallback).fail(failureCallback);
     $("#trips").toggle(false);
   });
-};
 
-// SHOW each trip--id grabbed from button html
-var showCallback = function(response) {
-  console.log("Success!");
-  console.log(response);
+  // SHOW each trip--id grabbed from button html
+  var showCallback = function(response) {
+    console.log("Success!");
+    console.log(response);
 
-  var tripData = response;
+    var tripData = response;
 
-  var showTemplate = _.template($('#show-trip-template').html());
-  var showHtml = showTemplate( { data: tripData } );
-  $('#show-page').html(showHtml);
-
-  $(".reserve-start").click(function(event) {
-    var reserveTemplate = _.template($('#reserve-trip-template').html());
-    $('#show-page').append(reserveTemplate);
-  });
-};
-
-var failureCallback = function() {
-  console.log("Fail :(");
-  $("#errors").html("<h1>Your AJAX request failed!</h1>");
+    var showTemplate = _.template($('#show-trip-template').html());
+    var showHtml = showTemplate( { data: tripData } );
+    $('#show-page').html(showHtml);
+    $('#show-page').data("id", response.id); // attach trip id to show page
+  };
 };
 
 //***********************ONCE DOM IS READY************************
 
 $(document).ready(function() {
+
   $("#trips").toggle(false); //to turn trips 'off' on load
 
   $("#load").click(function(event) {
     $.get(baseUrl, indexCallback).fail(failureCallback);
     $("#trips").toggle();
   });
+
+  $('#show-page').on("submit", "form", (function(event) {
+    event.preventDefault();
+    console.log("in submit form");
+    // console.log($('#show-page').data().id); //shows 1
+    var tripId = $('#show-page').data().id;
+    var reserveUrl = baseUrl + "/" + tripId.toString() + "/reserve";
+    console.log(reserveUrl);
+
+    var formData = $(this).serialize();
+    console.log(formData);
+
+    $.post(reserveUrl, formData, function(response) {
+      console.log("success!");
+      alert("Reservation added!");
+    });
+    this.reset();
+    return false;
+  }));
 });
