bae5c706ae75722141ca1dbbcaaa897ca4b87e06
laura <lmelgarejos@unal.edu.co>
2017-05-25 23:01:01 -0700

    Added error handling for the response

--- trek.js
+++ trek.js
@@ -2,42 +2,57 @@
 var url = 'https://trektravel.herokuapp.com/trips';
 
 var callBack = function(response){
-  console.log('Success!');
-  console.log(response);
+  // console.log('Success!');
+  // console.log(response);
 
-  var target = $('#listTrips');
   for (var i = 0; i < response.length; i++) {
     trip = response[i];
-    target.append('<li>' + trip['name'] + '</li>')
+    $('#listTrips').append('<li>' + trip['name'] + '</li>');
+
+    // if (trip['name'] === null ){
+    //   break;
+    // } else {
+    //   $('#listTrips').append('<li>' + trip['name'] + '</li>');
+    // };
   }
+};
+
+var failureCallback = function(){
+  console.log("did not work :()");
+  $('#errors').html("<h1>AJAX request failed!</h1>");
 };
 
 var clickHandler = function() {
   $.get(url, callBack);
+  $.get(url).success(callBack).fail(failureCallback);
 };
 
 $(document).ready(function() {
-
   $('#load').click(clickHandler);
-
-}); // final
-
+}); // final documento listo.
 
 
-
-
-
-
-
-// $.get(url,
-//   function(response){
-//     console.log('success!');
-//     console.log(response);
-//   })
-//   .fail(function(){
-//     console.log('failure');
-//   })
-//   .always(function(){
-//     console.log('always even if we have success or failure');
-//   });
-// );
+// $(document).ready(function() {
+//
+//   var clickHandler = function(){
+//     $.get(url,
+//       function(response){
+//         console.log('success!');
+//         console.log(response);
+//
+//         for (var i = 0; i < response.length; i++) {
+//           trip = response[i];
+//           $('#listTrips').append('<li>' + trip['name'] + '</li>');
+//         }
+//       })
+//       .fail(function(){
+//         console.log('failure');
+//       })
+//       .always(function(){
+//         console.log('always even if we have success or failure');
+//       });
+//   };
+//
+//   $('#load').click(clickHandler);
+//
+// }); // final
