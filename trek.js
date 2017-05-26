650be32e7836e26ce5382b287f1e81531de12da5
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-05-26 11:55:41 -0700

    trying to get clicks to work

--- trek.js
+++ trek.js
@@ -1,12 +1,14 @@
+/*****Return List of All Trips******/
 var url= "https://trektravel.herokuapp.com/trips";
 
 var allTemplate = _.template($('#all-trips-template').html());
 
-var successCallback = function(response) {
+var successCallback = function(event) {
   var target = $('#all-trips');
-  for (var i = 0; i < response.length; i++) {
+
+  for (var i = 0; i < event.length; i++) {
     var generatedHTML = allTemplate({
-      allData: response[i]
+      allData: event[i]
     });
     target.append($(generatedHTML));
   }
@@ -16,19 +18,40 @@ var failureCallback = function() {
   $('#errors').html('<h2>Apologies, it appears the list of trips is not available right now. </h2>');
 };
 
-var allClickHandler = function(event) {
+var clickHandler = function() {
+  alert($(this).attr("id")); // returns allLoad
   $.get(url, successCallback).fail(failureCallback);
 };
 
-/***********/
-// var singleTemplate = _.template($('#single-trip-template').html());
+/*****Return Single Trip Info******/
+var singleTemplate = _.template($('#single-trip-template').html());
 
+var successSingleCallback = function(event) {
+  var target = $('#single-trip');
 
+  for (var i = 0; i < event.length; i++) {
+    var generatedSingleHTML = singleTemplate({
+      singleData: event[i]
+    });
+    target.append($(generatedSingleHTML));
+  }
+};
 
-/***********/
+var failureSingleCallback = function() {
+  $('#errors').html('<h2>Apologies, it appears this trip info is not available right now. </h2>');
+};
+
+var singleClickHandler = function(event) {
+  console.log("WE HERE");
+  var id = event.target.id;
+  var singleURL = url + "/" + id;
+  $.get(singleURL, successSingleCallback).fail(failureSingleCallback);
+};
+
+/*****Execute above code on Click******/
 $(document).ready(function() {
-  $('#allLoad').click(allClickHandler);
-  //$('#singleLoad').click(singleClickHandler);
+  $('#allLoad').click(clickHandler);
+  $('#all-trips').on('click', '<%- allData.id %>', singleClickHandler);
 });
 
 /* would like some error handling to get rid of the ugly
