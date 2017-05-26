6d8926fbbc12b98de88598c8c682d44aa17ea010
Andrea Valliere <amvalliere@gmail.com>
2017-05-26 02:57:08 -0700

    Gave trip ID to buttons. Made click event for buttons - not functioning yet

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ var successCallback = function(response) {
     // append is going to cause the button to add on all the trips again every time its clicked
     $("#trips").append($(generatedHtml));
   }
+
+  tripsDetailTemplate = _.template($('#trips-detail-template').html());
+
 };
 
 var failureCallback = function() {
@@ -23,11 +26,30 @@ var clickHandler = function(event) {
   $.get(url, successCallback).fail(failureCallback)
 };
 
+var clickHandlerDetails = function(event) {
+  // clear the "All Trips" list
+  $("#trips").empty();
+  // make API call for specific id (id passed from button)
+  // render template
+  console.log("hi");
+};
+
 // to get around scope
 var tripsTemplate;
+var tripsdetailTemplate;
 
 $(document).ready(function() {
   tripsTemplate = _.template($('#trips-item-template').html());
 
   $('#load').click(clickHandler);
+  $('.button.trip-detail').click(clickHandlerDetails);
+
+  // trying to select class
+  // $(".button.trip-detail").on('click', function(){
+  //  console.log("yo");
+});
+
+  // $(document).ready(function() {
+  //   tripsDetailTemplate = _.template($('#trips-detail-template').html());
+  // });
 });
