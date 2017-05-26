84d3338e0d6939698a671615c8e0b800acd061a1
Danielle Birbal <birbalds@gmail.com>
2017-05-26 10:11:19 -0700

    Wave 3 Completed

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var failureCallback = function() {
 var allTrips = function(response){
   for (var i = 0; i < response.length; i++) {
     var trip = response[i];
-    $("#trips").append("<li class='oneTrip' id='" + trip['id'] + "'>"  + trip['name'] + "</li>");
+    $("#trips").append("<li class='oneTrip' id='" + trip['id'] + "'>"  + trip['name'] +"</li>").append("<button class='reserveBtn' id='resBtn" + trip['id'] + "'>Make Reservation</button>");
   }
 };
 
@@ -37,52 +37,55 @@ var clickHandler = function(){
 
         $.get(tripURL, tripData).fail(failureCallback);
       }
+
 };
-
-$('form').submit(function(e) {
-  // By default, the form will attempt to do it's own local POST so we want to prevent that default behavior
-  e.preventDefault();
-
-  var url = $(this).attr("action"); // Retrieve the action from the form
-  var formData = $(this).serialize();
-
-  $.post(url, formData, function(response){
-    $('#message').html('<p> Reservation Made! </p>');
-
-    // What do we get in the response?
-    console.log(response);
-  });
-});
-
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-$('#reserveBtn').onclick = function() {
-    $('#popUpReserve').style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-$('span.close').onclick = function() {
-    $('#popUpReserve').style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == $('#popUpReserve')) {
-        $('#popUpReserve').style.display = "none";
-    }
-}
 
 $(document).ready(function() {
   tripTemplate = _.template($('#trip-template').html());
 
+  $('form').submit(function(e) {
+    // By default, the form will attempt to do it's own local POST so we want to prevent that default behavior
+    e.preventDefault();
+
+    var url = $(this).attr("action"); // Retrieve the action from the form
+    var formData = $(this).serialize();
+
+    $.post(url, formData, function(response){
+      $('#message').html('<p> Reservation Made! </p>');
+
+      // What do we get in the response?
+      console.log(response);
+    });
+  });
+
+  // Get the modal
+  var modal = document.getElementById('popUpReserve');
+
     $.get(url, allTrips).fail(failureCallback);
 
     $('#trips').on('click', 'li.oneTrip', clickHandler);
+
+    // When the user clicks on the button, open the modal
+    $('body').on('click','.reserveBtn', function() {
+
+        modal.style.display = "block";
+        document.getElementById("reserveForm").action = "https://trektravel.herokuapp.com/trips/" + this.id + "/reserve";
+        document.getElementById("trip_id").value = this.id.replace(/\D/g,'');
+        console.log($('#trip_id').val());
+        // console.log($('#reserveForm').val());
+        // $('form').form.elements["trip_id"].value = $('.reserveBtn').id;
+    });
+
+    // When the user clicks on <span> (x), close the modal
+    $('body').on('click', '.close', function() {
+        modal.style.display = "none";
+    });
+
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.click = function(event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+    };
 });
