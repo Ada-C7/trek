63eea55902df476617eec217aaa8bb95e2e88fc8
laura <lmelgarejos@unal.edu.co>
2017-05-26 12:13:16 -0700

    Added underscore to render the template

--- trek.js
+++ trek.js
@@ -1,58 +1,31 @@
 
-var url = 'https://trektravel.herokuapp.com/trips';
-
-var callBack = function(response){
-  // console.log('Success!');
-  // console.log(response);
-
-  for (var i = 0; i < response.length; i++) {
-    trip = response[i];
-    $('#listTrips').append('<li>' + trip['name'] + '</li>');
-
-    // if (trip['name'] === null ){
-    //   break;
-    // } else {
-    //   $('#listTrips').append('<li>' + trip['name'] + '</li>');
-    // };
-  }
-};
-
-var failureCallback = function(){
-  console.log("did not work :()");
-  $('#errors').html("<h1>AJAX request failed!</h1>");
-};
-
-var clickHandler = function() {
-  $.get(url, callBack);
-  $.get(url).success(callBack).fail(failureCallback);
-};
-
 $(document).ready(function() {
+
+  var url = 'https://trektravel.herokuapp.com/trips';
+
+  var successCallBack = function(response){
+    console.log('Success!');
+    
+    var templateTrips = _.template($('#infoTrip').html());
+
+    for (var i = 0; i < response.length; i++){
+      var generatedHTML = templateTrips({
+        data: response[i]
+      });
+      $('#listTrips').append($(generatedHTML));
+    }
+  };
+
+  var failureCallback = function(){
+    console.log("did not work :()");
+    $('#errors').html("<h1>AJAX request failed!</h1>");
+  };
+
+  var clickHandler = function() {
+    $.get(url, successCallBack);
+    // $.get(url).success(successCallBack).fail(failureCallback);
+  };
+
   $('#load').click(clickHandler);
+
 }); // final documento listo.
-
-
-// $(document).ready(function() {
-//
-//   var clickHandler = function(){
-//     $.get(url,
-//       function(response){
-//         console.log('success!');
-//         console.log(response);
-//
-//         for (var i = 0; i < response.length; i++) {
-//           trip = response[i];
-//           $('#listTrips').append('<li>' + trip['name'] + '</li>');
-//         }
-//       })
-//       .fail(function(){
-//         console.log('failure');
-//       })
-//       .always(function(){
-//         console.log('always even if we have success or failure');
-//       });
-//   };
-//
-//   $('#load').click(clickHandler);
-//
-// }); // final
