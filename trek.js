fa8d3ef9abd2c280397756190b13f4ff7d513b77
Haby Randall <sow.haby@gmail.com>
2017-05-26 10:39:09 -0700

    Trying new methods with help from Ashton and Rana to show trips

--- trek.js
+++ trek.js
@@ -1,39 +1,53 @@
-$(document).ready(function() {
-var url = 'https://trektravel.herokuapp.com/trips';
 
-var successCallback = function(response) {
+
+
+var homeSuccessCallback = function(response) {
   console.log("Success");
+  // console.log(response);
+
+var homeTemplate = _.template($('#trips-template').html());
+  // var target = $('#trips-body');
+  //
   console.log(response);
-
-  var target = $('#trips');
-
-  for (var i = 0; i < response.length; i++) {
-    trip = response[i];
-    var row = $('<tr></tr>');
-    var name = $('<td><a href="#" id=' + trip.id + '>' + trip.name + '</a></td>');
-    var continent = $('<td>' + trip.continent + '</td>');
-    var weeks = $('<td' + trip.weeks + '</td>');
-    row.append(name, continent, weeks);
-    target.append(row);
-  }
+   for (var i = 0; i < response.length; i++) {
+     console.log(response[i]);
+     var generatedHtml = homeTemplate({
+       trip: response[i],
+     });
+     $('#trip-list').append(generatedHtml);
+   }
 };
 
-$('#load').click( function() {
-  $('#trips').empty();
-  $('#load').hide();
-  $.get(url, successCallback);
-});
+  //   trip = response[i];
+  //   var row = $('<tr></tr>');
+  //   var name = $('<td><a href="#" id=' + trip.id + '>' + trip.name + '</a></td>');
+  //   var continent = $('<td>' + trip.continent + '</td>');
+  //   var weeks = $('<td' + trip.weeks + '</td>');
+  //   row.append(name, continent, weeks);
+  //   target.append(row);
+
+// $('#load').click( function() {
+//   // $('#trips-body').empty();
+//   // $('#load').hide();
+//   $.get(url, homeSuccessCallback);
+// });
+
+// $('#trips-body').on('click', 'a', function(e) {
+//   e.preventDefault();
+//   $('#trips-body').show();
+//   var tripUrl = $(this).attr('href');
+// });
 
 var failureCallback = function() {
-  console.log("Didn't work: (");
+  console.log("Didn't work:");
   $("#errors").html("<h1>Ajax request failed!</h1>");
 };
 
-var clickHandler = function() {
-  $.get(url, successCallback).fail(failureCallback);
+var homeClickHandler = function() {
+var url = 'https://trektravel.herokuapp.com/trips';
+  $.get(url, homeSuccessCallback).fail(failureCallback);
 };
 
 $(document).ready(function() {
-  $('#load').click(clickHandler);
-});
+  $('#load').click(homeClickHandler);
 });
