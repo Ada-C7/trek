ddc06400c3d3d564da300ec0ed2a7ac0ba9d5f4c
Ashtn <ashtonehorton@gmail.com>
2017-05-27 11:45:38 -0700

    added ability to request reservation, with successful alert

--- index.js
+++ index.js
@@ -1,58 +1,83 @@
 $(document).ready(function(){
-
-$('#explore').click(indexClickHandler);
-$('#trip-index').on('click', '#trip-link', showClickHandler);
+  $('#explore').click(indexClickHandler);
 });
 
 
-// ~~~~~~~~~~~~~~~~~~~~~~ INDEX ~~~~~~~~~~~~~~~~~~~~~~~~ //
+// ####################### Trip Index/List ####################### //
 
 var indexClickHandler = function(){
-     url = 'https://trektravel.herokuapp.com/trips';
-    $.get(url, indexSuccessCallback).fail(failureCallback);
+  url = 'https://trektravel.herokuapp.com/trips';
+  $.get(url, indexSuccessCallback).fail(failureCallback);
 
 };
 
 
 var indexSuccessCallback = function(response){
+  // TODO find a way to clear show info if index button is pressed again
+  $('#trip-index').empty();
+
 
   var tripIndexTemplate = _.template($('#trip-index-template').html());
 
   for(var i=0;i<response.length;i++){
     var generatedHtml = tripIndexTemplate({
-      trip: response[i]
+      trip: response[i],
     });
-    $('#trip-index').append($(generatedHtml));
+    $('#trip-index').append(generatedHtml);
   }
+  $('#trip-index').on('click', '#trip-link', showClickHandler);
 };
 
 
 var failureCallback = function(){
-    $('#errors').html("<h1>Oops Something Went Wrong</h1>");
+  $('#errors').html("<h1>Oops Something Went Wrong</h1>");
 };
 
 
-// ~~~~~~~~~~~~~~~~~~~~~~ SHOW ~~~~~~~~~~~~~~~~~~~~~~~~ //
+// ####################### Trip Show/Details ####################### //
 
 var showClickHandler = function(event){
 
   var tripId = $(event.target)[0].getAttribute('id');
 
-     url = 'https://trektravel.herokuapp.com/trips/' + tripId;
-    $.get(url, showSuccessCallback).fail(failureCallback);
+  url = 'https://trektravel.herokuapp.com/trips/' + tripId;
 
+  $.get(url, showSuccessCallback).fail(failureCallback);
 };
 
 var showSuccessCallback = function(response){
-console.log(response);
+
   var tripShowTemplate = _.template($('#trip-show-template').html());
 
-    var generatedHtml = tripShowTemplate({
-      trip: response
-    });
+  var url = 'https://trektravel.herokuapp.com/trips/'+response.id+'/reserve';
 
-    // TODO what is the difference between .append and .html
-    $('#trip-index').html($(generatedHtml));
-    // $('#trip-show').append($(generatedHtml));
+  var generatedHtml = tripShowTemplate({
+    trip: response,
+    url: url
+  });
+
+  // TODO what is the difference between .append and .html
+  $('#trip-index').html($(generatedHtml));
+  // $('#trip-show').append($(generatedHtml));
 
 };
+
+// ####################### Reservation ####################### //
+
+
+// this needs to bind to something that exists on the page at the time of the initial load
+
+$('#trip-index').on('click', 'button', function(e) {
+
+  e.preventDefault();
+
+  var url = $('form').attr("action");
+  var formData = $('form').serialize();
+
+  $.post(url, formData, function(response){
+
+    alert("Your reservation has been requested!");
+    $('form').empty();
+  })
+  .fail(failureCallback);
+});
