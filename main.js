32644931d970c8f790693abfd62091a6106b96eb
Ting Wong <hellotingwong@gmail.com>
2017-05-28 16:35:50 -0700

    added css

--- main.js
+++ main.js
@@ -6,10 +6,12 @@ var singleTripTemplate;
 var displayFormTemplate;
 
 $('#trip-list').hide();
+$('#trip-header').hide();
 
 var successCallback = function(response) {
   console.log("Success!");
   console.log(response);
+  $('#trip-header').show();
   $('#load').hide();
   $('#trip-list').show();
 
@@ -22,22 +24,34 @@ var successCallback = function(response) {
   $('.trip-details').click(tripDetailHandler);
 };
 
+var backToListCallback = function() {
+  console.log("WE ARE IN BACKTOLIST CALLBACK!");
+  $('#display-form').hide();
+  $('#trip-header').show();
+  $('#single-trip-detail').hide();
+  $('#load').hide();
+  $('#trip-list').show();
+  $('.trip-details').click(tripDetailHandler);
+};
+
 var singleTripCallback = function(response) {
   console.log("Success!");
   console.log(response);
+  $('#trip-header').hide();
   $('#trip-list').hide();
   $('#load').hide();
   var generatedHTML = singleTripTemplate({
     trip: response
   });
   $('#single-trip-detail').html(generatedHTML);
-  $('#all-trips-button').click(clickHandler);
+  $('#single-trip-detail').show();
+  $('#all-trips-button').click(backToListCallback);
   $('#display-form-button').click(displayFormHandler);
 };
 
 var bookTripCallback = function(response){
   console.log(response);
-  $('#reservation-message').html('<p> Trip Reserved! </p>');
+  $('#reservation-message').html('Trip Reserved!');
 };
 
 var failureCallback = function() {
@@ -49,25 +63,33 @@ var clickHandler = function(event) {
   $.get(url, successCallback).fail(failureCallback);
 };
 
-var tripDetailHandler = function(event){
+var tripDetailHandler = function(event) {
+  console.log("CLICKED");
   var tripID = $(this).attr("data-trip-id");
   var detailURL = url + tripID;
+  console.log(detailURL);
   $.get(detailURL, singleTripCallback).fail(failureCallback);
 };
 
 var displayFormHandler = function(event){
-  var generatedHTML = displayFormTemplate({});
+  $('#display-form').show();
+  var generatedHTML = displayFormTemplate({
+    id: $(this).attr("data-reservation-id")
+  });
   $('#display-form').html(generatedHTML);
-  // $('#all-trips-button').click(clickHandler);
   $('#display-form-button').hide();
-  $('#submit-trip-reservation').click(bookTripHandler);
+  $('.button.submit-trip-reservation').click(bookTripHandler);
 };
 
 var bookTripHandler = function(event){
-  var tripID = $(this).attr("data-trip-id");
-  var submitURL = url + tripID + "/reserve"
-  var formData = $(this).serialize();
-  $.post(submitURL, formData, bookTripCallback)};
+  event.preventDefault();
+  var submitURL = $(this).parents("form").attr("action");
+  var formData = $(this).parents("form").serialize();
+  console.log("THIS IS FORM DATA");
+  console.log(formData);
+  $.post(submitURL, formData, bookTripCallback);
+  $('#display-form').hide();
+};
 
 $(document).ready(function() {
   tripTemplate = _.template($('#trip-template').html());
