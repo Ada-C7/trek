55b62489e4a2a2a9c50c2e231a203d26679ff245
laura <lmelgarejos@unal.edu.co>
2017-05-29 13:10:19 -0700

    Wave 2 finished

--- trek.js
+++ trek.js
@@ -2,18 +2,25 @@
 $(document).ready(function() {
 
   var url = 'https://trektravel.herokuapp.com/trips';
+  var tripsTemplate = _.template($('#trips-template').html());
+  var oneTripsTemplate = _.template($('#one-trip-template').html());
 
   var successCallBack = function(response){
     console.log('Success!');
-    
-    var templateTrips = _.template($('#infoTrip').html());
+    console.log(response);
 
     for (var i = 0; i < response.length; i++){
-      var generatedHTML = templateTrips({
+      console.log(response[i]);
+
+      var generatedHTML = tripsTemplate({
         data: response[i]
       });
-      $('#listTrips').append($(generatedHTML));
+      console.log(generatedHTML);
+      $('#showInfo').append($(generatedHTML));
+
     }
+    $('.more-info').click(clickShowInfoHandler);
+
   };
 
   var failureCallback = function(){
@@ -21,11 +28,28 @@ $(document).ready(function() {
     $('#errors').html("<h1>AJAX request failed!</h1>");
   };
 
-  var clickHandler = function() {
+  var clickButtonHandler = function() {
     $.get(url, successCallBack);
     // $.get(url).success(successCallBack).fail(failureCallback);
   };
 
-  $('#load').click(clickHandler);
+  var successSingleTrip = function(response){
+    var id = response.id;
+    var generatedHTML = oneTripsTemplate({
+      data: response
+    });
+    $("#" + id).append($(generatedHTML));
+  };
+
+  var clickShowInfoHandler = function(event){
+    var tripId = event.currentTarget.dataset.id;
+
+
+    $.get('https://trektravel.herokuapp.com/trips/' + tripId, successSingleTrip);
+
+  };
+
+  $('#load').click(clickButtonHandler);
+  // $('.more-info').click(clickShowInfoHandler);
 
 }); // final documento listo.
