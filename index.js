b5a59c1e9c6853f3951aa6e16a98b582ab3a376b
Hyunji Kim <ricecakemonster@gmail.com>
2017-05-29 13:47:04 -0700

    sorting functions working correctly

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+
 var buildUrl = function(option){
   return "https://trektravel.herokuapp.com/trips/" + option;
 };
@@ -9,7 +10,10 @@ $(document).ready(function() {
   });
 
   $("#button").click(tripsClickHandler);
+  $("#create_trip").click(createTripClicHandler);
 
+
+  // ///============= Using Form instead of Dropdown menu================
   // $(".category-form").submit(function(e){
   //   e.preventDefault();
   //   var formData = $(this).serialize();
@@ -17,106 +21,81 @@ $(document).ready(function() {
   //   var url = buildUrl(urlArry[0]+"?query="+urlArry[1]);
   //   $.get(url, tripsSuccessCallback).fail(failureCallback);
   // });
-//
-// ---------------------------------------------
-//
-//
-// /////============= Using Form instead of Dropdown menu================
-// // $(".category-form").submit(function(e){
-// //   e.preventDefault();
-// //   var formData = $(this).serialize();
-// //   var urlArry = formData.split("=")
-// //   var url = buildUrl(urlArry[0]+"?query="+urlArry[1]);
-// //   $.get(url, tripsSuccessCallback).fail(failureCallback);
-// // });
-// /////==================================================================
-//
-// $(".dropdown-item").on("click", function(){
-//   console.log(this);
-//   var category = $(this).attr("data-category");
-//   if (category == "budget"){
-//     var url = "budget?query=" + $(this).html();
-//     console.log(url);
-//   } else if (category == "weeks"){
-//     var url = "weeks?query=" + $(this).html();
-//   } else {
-//     var url = "continent?query=" + $(this).html();
-//   }
-//   categoryClickHandler(url);
-// });
-//
-// });
-//
-// var categoryClickHandler = function(url){
-// console.log(buildUrl(url));
-// // $.get(url, tripsSuccessCallback).fail(failureCallback);
-// }
-//
+  // ///==================================================================
 
-// -----------------------------------------------
-
-  $("#category-budget [href]").on("click", function(){
-    console.log(this);
-    var url = "budget?query=" + $(this).html();
+  $("[href]").on("click", function(){
+    var category = $(this).attr("data-category");
+    if (category == "budget"){
+      var url = "budget?query=" + $(this).html();
+    } else if (category == "weeks"){
+      var url = "weeks?query=" + $(this).html();
+    } else {
+      var url = "continent?query=" + $(this).html();
+    }
     categoryClickHandler(url);
+  });
+  //
 });
 
-
-  // $("#category-budget [href]").click(categoryClickHandler)
-  $("#category-weeks [href]").click(categoryClickHandler)
-  $("#category-continent [href]").click(categoryClickHandler)
-
-  // $("[href]").click(categoryClickHandler)
-  // $(".dropdown-content").on("click", "href", categoryClickHandler);
-});
-
-var categoryClickHandler = function(url){
-  console.log(buildUrl(url));
-  // $.get(url, tripsSuccessCallback).fail(failureCallback);
-}
-
-var createTripClicHandler = function(){
+var categoryClickHandler = function(data){
   $("#display").empty();
-  var createTripTemplate = _.template($("#create-trip-template").html());
-  var generatedHtml = createTripTemplate({
-  });
-  $("#display").append($(generatedHtml));
-  $("#create-trip-form").submit(function(event) {
-    event.preventDefault();
+  $("#options").empty();
 
-    var url = $(this).attr("action");
-    var formData = $(this).serialize();
-
-    $.post(url, formData, function(response){
-      $('#message').html("<p> New trip is created! </p>");
-    })
-    .fail(function(){
-      $("#message").html("<p> Failed to create a trip!</p>")
-    })
-    $("#display").empty();
-  });
-
+  var url = buildUrl(data)
+  // console.log(url);
+  $.get(url, tripsSuccessCallback).fail(failureCallback);
 }
 
 var tripsClickHandler = function(){
   $("#display").empty();
   var url = buildUrl("");
-  console.log(url);
+  // console.log(url);
   $.get(url, tripsSuccessCallback).fail(failureCallback);
 }
 
-var tripsSuccessCallback = function(response){
-  // $(".category-form").hide();
-  $('#message').empty();
+var displayTrips = function(data){
   var tripsTemplate = _.template($("#trips-template").html());
-  console.log(response[0]);
-  for (var i = 0; i < response.length; i++) {
 
+  for (var i = 0; i < data.length; i++) {
     var generatedHtml = tripsTemplate({
-      data: response[i],
+      data: data[i],
     });
     $("#display").append($(generatedHtml));
+  }
+};
 
+var tripsSuccessCallback = function(response){
+  // $(".category-form").hide();  // For using forms for categories instead
+  $('#message').empty();
+  if (response != null){
+    $("#options").empty();
+    var sortedResponse = response;
+    displayTrips(sortedResponse);
+
+    if (response[0].cost != null){
+      $("#options").append("<section class='button tiny'><button id='sort-budget'> Sort by Budget </button></section>");
+      $("#sort-budget").on("click", function(){
+
+        sortedResponse = _.sortBy( response, "cost")
+        $("#display").empty();
+
+        displayTrips(sortedResponse);
+      })
+    }
+    if (response[0].weeks != null){
+
+      $("#options").append("<section class='button tiny'><button id='sort-length'> Sort by Length </button></section>")
+
+      $("#sort-length").on("click", function(){
+
+        sortedResponse = _.sortBy( response, "weeks")
+        $("#display").empty();
+        displayTrips(sortedResponse);
+      })
+    }
+
+  } else {
+    $("#message").html("<p> No trips to show! </p>");
   }
   $(".trip").on("click", "button",   (function(){
     var tripId = $(this).html().replace("Detail of Trip ", "");
@@ -125,6 +104,7 @@ var tripsSuccessCallback = function(response){
   }));
 
 };
+
 
 
 var tripSuccessCallback = function(response){
@@ -147,6 +127,29 @@ var tripSuccessCallback = function(response){
     .fail(function(){
       $("#message").html("<p> Failed to make a reservation!</p>")
     })
+  });
+
+}
+
+var createTripClicHandler = function(){
+  $("#display").empty();
+  var createTripTemplate = _.template($("#create-trip-template").html());
+  var generatedHtml = createTripTemplate({
+  });
+  $("#display").append($(generatedHtml));
+  $("#create-trip-form").submit(function(event) {
+    event.preventDefault();
+
+    var url = $(this).attr("action");
+    var formData = $(this).serialize();
+
+    $.post(url, formData, function(response){
+      $('#message').html("<p> New trip is created! </p>");
+    })
+    .fail(function(){
+      $("#message").html("<p> Failed to create a trip!</p>")
+    })
+    $("#display").empty();
   });
 
 }
