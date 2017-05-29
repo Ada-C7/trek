4cc15f1132b8ec55d8d5c55cd7399a2d4d33383e
Tamiko Terada <tamikodevo@gmail.com>
2017-05-28 22:25:22 -0700

    add new trip function

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 // single-page app with jQuery + AJAX
 
-var baseUrl = "https://trektravel.herokuapp.com/trips"; // Trek API
-
 var failureCallback = function() {
   console.log("Fail :(");
   $("#errors").html("<h1>Your AJAX request failed!</h1>");
 };
 
-// INDEX ~ displays all trips
+// INDEX ~ callback for all trips + template
 var indexCallback = function(response) {
   console.log("Success!");
   console.log(response);
@@ -20,7 +18,7 @@ var indexCallback = function(response) {
   $('#index-page').append(all_trips);
 };
 
-// SHOW ~ displays each trip w/ more details
+// SHOW ~ callback for each trip + template
 var showCallback = function(response) {
   console.log("Success!");
   console.log(response);
@@ -33,20 +31,26 @@ var showCallback = function(response) {
   $('#show-page').data("id", response.id); // attach trip id to show page
 };
 
+// NEW ~ template for adding a new trip
+var newTemplate = _.template($('#new-trip-template').html());
+$('#new-page').append(newTemplate);
+
 //***********************ONCE DOM IS READY************************
 
 $(document).ready(function() {
+  var baseUrl = "https://trektravel.herokuapp.com/trips"; // Trek API
 
+  // INDEX ~ event handler for button
   $("#index-page").toggle(false); // start with trips toggled to 'off'
 
-  $("#load").click(function(event) {
+  $("#load-button").click(function(event) {
     $.get(baseUrl, indexCallback).fail(failureCallback);
     $("#index-page").toggle();
   });
 
-  // SHOW Event Handler: displays single trip detail
+  // SHOW ~ event handler for link
   $("#index-page").on("click", "a", function(event) {
-    console.log("success");
+    console.log("success!");
     event.preventDefault();
     var tripId = $(this).data().id;
     var showUrl = baseUrl + "/" + tripId.toString();
@@ -56,10 +60,10 @@ $(document).ready(function() {
     return false;
   });
 
-  // RESERVE Event Handler: submits form for reservation
+  // RESERVE ~ event handler for form
   $('#show-page').on("submit", "form", (function(event) {
     event.preventDefault();
-    console.log("in submit form");
+    console.log("success!");
     // console.log($('#show-page').data().id); //shows 1
     var tripId = $('#show-page').data().id;
     var reserveUrl = baseUrl + "/" + tripId.toString() + "/reserve";
@@ -71,8 +75,41 @@ $(document).ready(function() {
     $.post(reserveUrl, formData, function(response) {
       console.log("success!");
       alert("Reservation added!");
+      console.log("Form submitted!");
     });
+
     this.reset();
     return false;
   }));
+
+  // NEW ~ template and event handler for button
+  $("#new-page").toggle(false); // start with trips toggled to 'off'
+
+  $("#new-button").click(function(event) {
+    console.log("success!");
+    $("#new-page").toggle();
+  });
+
+  // NEW ~ event handler for form
+  $('#new-page').on("submit", "form", (function(event) {
+    event.preventDefault();
+    console.log("success!");
+
+    var newData = $(this).serialize();
+    var newUrl = this.action;
+    console.log(newUrl);
+
+    console.log(newData);
+
+    $.post(newUrl, newData, function(response) {
+      console.log(response);
+      console.log("success!");
+      alert("New trip added");
+      console.log("Trip added!");
+    });
+
+    this.reset();
+    return false;
+  }));
+
 });
