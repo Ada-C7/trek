7a358403ada60f41a88a654ec34b237fadfdb32b
Tehut Getahun <tehut.m.g@gmail.com>
2017-05-29 01:14:19 -0700

    Individual trip get request and showl

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 
-var hideItem = function (itemId, targetId){
-  $(targetId).click(function(){
-    console.log('here');
-  });
-};
-
 $(document).ready(function() {
+
   var tripTemplate = _.template($('#trip-template').html());
   var aTripTemplate = _.template($('#single-trip-template').html());
 
@@ -26,21 +21,46 @@ $(document).ready(function() {
       var aTrip = tripTemplate({
         data: allTrips[j]
       });
+
       linkId = 'details' + j,
       nameId = 'trip' + j;
 
+      $('#mainView').append(aTrip)
 
-      $('#please').append(aTrip);
+      $('a.tripName:last').attr("id", allTrips[j].id).attr("href",'#' + linkId);
 
-      $('a.tripName:last').attr("id", nameId).attr("href",linkId);
-      console.log(linkId);
-
-      $(nameId).click(function (){
-      });
-        // hideItem(linkId, nameId);
       }
-  };
-  $('#all-trips').click( function (event){
-    $.get('https://trektravel.herokuapp.com/trips', successCallback);
+    };
+
+    var showTrip = function(response) {
+      var showTrip = aTripTemplate({
+        data: {
+          id: response.id,
+          name: response.name,
+          cost: response.cost,
+          weeks: response.weeks,
+          continent: response.continent,
+          category: response.category,
+          about: response.about
+        }
+      });
+            console.log(showTrip);
+            // this is where I put the individual trips
+      $('#mainView').html(showTrip);
+    };
+
+
+    $('#all-trips').click( function (event){
+      $.get('https://trektravel.herokuapp.com/trips', successCallback);
+    });
+// 
+    $('body').on ('click','a.tripName', function(event){
+      var  id = $(this).attr("id");
+      $.get(('https://trektravel.herokuapp.com/trips/' + id), showTrip);
+      // console.log(id);
+    });
+ $
+    $('body').on ('click','a.tripName', function(event){
+      $('#all-trips').text('Return to all trips')
+    });
   });
-});
