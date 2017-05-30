dd6e34dcc206f79f13fcec6aaf31d1d34d138225
Tehut Getahun <tehut.m.g@gmail.com>
2017-05-30 09:05:00 -0700

    successful input of reservation form

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ $(document).ready(function() {
 
   var tripTemplate = _.template($('#trip-template').html());
   var aTripTemplate = _.template($('#single-trip-template').html());
+  // var resTripTemplate = _.template($('#reservation-form-templa te').html());
+  // var reservationsTemplate = _.template($('#reservations-template').html());
+
+  var url = 'https://trektravel.herokuapp.com/trips';
 
   var successCallback = function (response) {
     var allTrips = [],
@@ -25,12 +29,23 @@ $(document).ready(function() {
       linkId = 'details' + j,
       nameId = 'trip' + j;
 
-      $('#mainView').append(aTrip)
-
-      $('a.tripName:last').attr("id", allTrips[j].id).attr("href",'#' + linkId);
-
-      }
+      $('#main-view').append(aTrip);
+      $('a.trip-name:last').attr("id", allTrips[j].id).attr("href",'#' + linkId);
+    }
     };
+
+    // Display Reservation form
+    // $('body').on ('click','#reservation-form-button', function(e){
+    //   e.preventDefault();
+    //   var id = $(this).attr('data-trip-id'),
+    //   reserveUrl = 'https://trektravel.herokuapp.com/trips/' + id +'/reserve'
+    //   resForm = resTripTemplate();
+    //   console.log(id);
+    //   $('#main-view').append($(resForm));
+    //   $('#reservation-form').attr('action', reserveUrl);
+
+    // $('#reservation-section').toggleClass("hidden")
+    // });
 
     var showTrip = function(response) {
       var showTrip = aTripTemplate({
@@ -44,23 +59,49 @@ $(document).ready(function() {
           about: response.about
         }
       });
-            console.log(showTrip);
-            // this is where I put the individual trips
-      $('#mainView').html(showTrip);
+      $('#main-view').html(showTrip);
+
+
+      $('#reservation-form').submit( function(e) {
+        e.preventDefault();
+        var id = response.id//$('reservation-complete-button').attr('data-trip-id');
+          console.log(id);
+
+        var reserveUrl = 'https://trektravel.herokuapp.com/trips/' + id+'/reserve',
+        // resForm = resTripTemplate(),
+        formData = $(this).serialize();
+        // reserveUrl = $(this).attr('action')
+        console.log(reserveUrl);
+        $.post(reserveUrl, formData, function(response){
+          $('#messages').prepend("<h3> Reservation Made! </h3>");
+
+        }).fail(function(){
+          $('#messages').prepend("<h3> Reservation Failed. </h3>");
+        });
+      });
     };
 
+// On Click Events
 
+// Show All Trips
     $('#all-trips').click( function (event){
-      $.get('https://trektravel.herokuapp.com/trips', successCallback);
+      $.get(url, successCallback);
     });
-// 
-    $('body').on ('click','a.tripName', function(event){
+
+//View a single trip
+    $('body').on ('click','a.trip-name', function(event){
       var  id = $(this).attr("id");
-      $.get(('https://trektravel.herokuapp.com/trips/' + id), showTrip);
-      // console.log(id);
+      $.get((url +'/'+ id), showTrip);
+
     });
- $
-    $('body').on ('click','a.tripName', function(event){
+ // Return to All Trips View
+    $('body').on ('click','a.trip-name', function(event){
       $('#all-trips').text('Return to all trips')
     });
+
+
+
+
+
+
   });
