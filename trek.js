11c50bf969b8220a85a37ff0562b2947f0d79b44
Haby Randall <sow.haby@gmail.com>
2017-05-30 18:28:37 -0700

    Deleted commented out lines not needed

--- trek.js
+++ trek.js
@@ -3,14 +3,10 @@
 
 var homeSuccessCallback = function(response) {
   console.log("Success");
-  // console.log(response);
 
   var homeTemplate = _.template($('#trips-template').html());
-  // var target = $('#trips-body');
-  //
-  // console.log(response);
+
   for (var i = 0; i < response.length; i++) {
-    //  console.log(response[i]);
     var generatedHtml = homeTemplate({
       trip: response[i],
     });
@@ -20,7 +16,6 @@ var homeSuccessCallback = function(response) {
 };
 
 var showSuccessCallback = function(response) {
-  // console.log(response);
   var showTemplate = _.template($('#trips-show-template').html());
 
   var url = 'https://trektravel.herokuapp.com/trips/'+ response.id +'/reserve';
@@ -64,30 +59,6 @@ var showClickHandler = function() {
 };
 
 
-// "https://trektravel.herokuapp.com/trips/" + trip.id + "/reserve"
-
 $(document).ready(function() {
   $('#load').click(homeClickHandler);
-  // $('#trips-show').on('click', '.trip-link', showClickHandler);
 });
-
-
-//   trip = response[i];
-//   var row = $('<tr></tr>');
-//   var name = $('<td><a href="#" id=' + trip.id + '>' + trip.name + '</a></td>');
-//   var continent = $('<td>' + trip.continent + '</td>');
-//   var weeks = $('<td' + trip.weeks + '</td>');
-//   row.append(name, continent, weeks);
-//   target.append(row);
-
-// $('#load').click( function() {
-//   // $('#trips-body').empty();
-//   // $('#load').hide();
-//   $.get(url, homeSuccessCallback);
-// });
-
-// $('#trips-body').on('click', 'a', function(e) {
-//   e.preventDefault();
-//   $('#trips-body').show();
-//   var tripUrl = $(this).attr('href');
-// });
